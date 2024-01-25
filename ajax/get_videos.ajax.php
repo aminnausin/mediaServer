9a33149eff75ae6dd1b6dbc57f8ab1ffb1e1f932
aminnausin <aminnausin@outlook.com>
2024-01-24 20:00:11 -0500

    Convert to API structure, use JSON data and perform html construction in JS

--- ajax/get_videos.ajax.php
+++ ajax/get_videos.ajax.php
@@ -1,55 +1,80 @@
 <?php
-$path = $_POST["directory"];
+require_once "../config.php";
+$postRaw = file_get_contents('php://input');
+$postData = json_decode($postRaw);
+
+$path = isset($postData->dir) ? '.' . RESOURCE_DIR . $postData->dir . '\\' : '.' . RESOURCE_DIR . RESOURCE_DEFAULT;
 $folderCount = 0;
+
+if(! is_dir($path)){
+    $error = 'Invalid Directory: "' . $path . '"';
+
+    echo json_encode(array("success"=>false, "result"=>"", "error"=>$error), JSON_UNESCAPED_SLASHES);
+    return;
+}
+
+$out = '';
+
+$folders = array();
+$currentFolder = array("name"=>"", "files"=>array());
 
 foreach ($iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS), RecursiveIteratorIterator::SELF_FIRST) as $file) {
     /** @var SplFileInfo $file */
 
     if ($file->isDir() && basename($file) != '.thumbs') {
+
+        if($folderCount != 0){
+            array_push($folders, $currentFolder);
+            $currentFolder["files"] = array();
+        }
         
-        $out = '
-        <div class="col-sm-12" style="padding: 2%;">
-            <div class="row mb-4">
-                <h4 class="col-sm-10">' . basename($file) . '</h4> 
-                <button class="col-sm-2 btn btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#dataTable-' . $folderCount . '-collapse">
-                    <i class="bi bi-list"></i>
-                    Hide Folder
-                </button>
-            </div>
-            <div class="collapse" id="dataTable-' . $folderCount . '-collapse">
-                <table class="vid-table table table-hover table-striped" id="dataTable-' . $folderCount . '">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Who knows?</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-            ';
+        $currentFolder["name"] = basename($file);
+        
+        
+        // $out .= '
+        // <div class="col-sm-12" style="padding: 2%;">
+        //     <div class="row mb-4">
+        //         <h4 class="col-sm-10">' . basename($file) . '</h4> 
+        //         <button class="col-sm-2 btn btn-light folder-toggle" id="dataTable-' . $folderCount . '-collapse-toggle" type="button" data-bs-toggle="collapse" data-bs-target="#dataTable-' . $folderCount . '-collapse">
+        //             <i class="bi bi-list"></i>
+        //             Show Folder
+        //         </button>
+        //     </div>
+        //     <div class="collapse show" id="dataTable-' . $folderCount . '-collapse" data-state="0">
+        //         <table class="vid-table hover stripe" id="dataTable-' . $folderCount . '" data-folder="' . basename($file) . '">
+        //             <thead>
+        //                 <tr>
+        //                     <th>Title</th>
+        //                     <!-- <th>Length</th> -->
+        //                     <th>Date</th>
+        //                 </tr>
+        //             </thead>
+        //             <tbody>
+        //     ';
 
-            if($folderCount != 0) $out = '
-                    </tbody>
-                </table>
-            </div>    
-        </div>
-        <hr>
-        ' . $out;
-
-        echo $out;
+        //     if($folderCount != 0) $out = '
+        //             </tbody>
+        //         </table>
+        //     </div>    
+        // </div>
+        // <hr>
+        // ' . $out; // if not first folder, then close the last folder
 
         $folderCount += 1;
     } else {
-        if ($file->getExtension() == 'mp4') {
-            echo('
+        if ($file->getExtension() == 'mp4' || $file->getExtension() == 'mkv' ) {
+            $fileData = array('name'=> (string) $file, 'title'=>basename($file), 'date' => filectime($file), 'formattedDate' => date("Y-m-d g:i A", filectime($file)));
+            array_push($currentFolder['files'], $fileData);
+            $out .= '
                 <tr>
                     <td class="vid-row" data-col="col-title" value="' . $file . '">' . basename($file) . '</td>
-                    <td></td>
+                    <!-- <td class="vid-row" data-col="col-length" value="0">Unimplemented</td> -->
+                    <td class="vid-row" data-col="col-date" value="' . filectime($file) . '">' . date("Y-m-d g:i A", filectime($file)) . '</td>
                 </tr>
-            ');
+                    
+            ';
         }
     }
 }
-
-
-echo $result;
+echo json_encode(array("success"=>true, "result"=>$folders, "error"=>""), JSON_UNESCAPED_SLASHES);
 ?>
