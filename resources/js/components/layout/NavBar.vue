18efe38ef5a505cf0e2500b55e0ff3936c2158e3
aminnausin <aminnausin@outlook.com>
2024-07-23 00:52:43 -0400

    new auth (messy)

--- resources/js/components/layout/NavBar.vue
+++ resources/js/components/layout/NavBar.vue
@@ -2,13 +2,14 @@
     import { storeToRefs } from 'pinia';
     import { useAuthStore } from '../../stores/AuthStore'
     import { ref, onMounted } from 'vue';
+    import { RouterLink } from 'vue-router';
     import UserDropdown from '../user/UserDropdown.vue';
 
     const authStore = useAuthStore();
-    const { user, auth, pageTitle, darkMode } = storeToRefs(authStore);
+    const { userData, pageTitle, darkMode } = storeToRefs(authStore);
     const showDropdown = ref(false);
-
-    const { toggleDarkMode } = authStore;
+    const username = ref('')
+    const { toggleDarkMode, cycleSideBar, auth } = authStore;
 
     const toggleDropdown = () => {
         showDropdown.value = !showDropdown.value;
@@ -16,7 +17,7 @@
 
     window.addEventListener('click', function(e){
         try {
-            if (!this.document.querySelector("#user_options").contains(e.target)){
+            if (!this.document.querySelector("#user_options")?.contains(e.target)){
                 showDropdown.value = false;
             } 
         } catch (error) {
@@ -25,8 +26,9 @@
     });
 
 
-    onMounted(() => {
-        toggleDarkMode(true);
+    onMounted(async () => {
+        await auth();
+        username.value = userData.value?.name ?? '';
     })
 </script>
 
@@ -38,31 +40,36 @@
                 <section id="user_options" class="group inline-block relative" data-dropdown-toggle="user_dropdown" aria-haspopup="true">
                     <button id="user_header" class="flex space-x-2 text-2xl text-slate-900 dark:text-white hover:text-orange-600 dark:hover:text-orange-600 items-center justify-center" @click="toggleDropdown">
                         
-                        <span id="user_name" v-if="auth">{{ user.username }}</span>
+                        <span id="user_name" v-if="username">{{ username }}</span>
                         <span id="user_name_unauth" v-else class="w-[10vw] text-right">Guest</span>
                         
-                        <img :src="user.avatar" class="h-7 w-7 rounded-full sm:mx-0 sm:shrink-0 ring-2 ring-orange-600/60 shadow-lg object-cover" alt="profile picture">
+                        <img :src="userData?.value?.avatar ?? '/storage/avatars/12345.jpg'" class="h-7 w-7 rounded-full sm:mx-0 sm:shrink-0 ring-2 ring-orange-600/60 shadow-lg object-cover" alt="profile picture">
                     </button>    
                     <UserDropdown v-if="showDropdown"/>
                 </section>
 
                 <section id="navbar-video" class="flex items-center space-x-2">
-                    <button id="btn-nav-folders" class="flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-red-300 hover:bg-red-200" aria-label="folders">
+                    <button v-if="$route.name === 'home'" id="btn-nav-folders" @click="cycleSideBar('folders')" class="flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-red-300 hover:bg-red-200" aria-label="folders">
                         <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" height="24" width="24" class="stroke-slate-900">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
                         </svg>
                     </button>
-                    <button v-if="auth" id="btn-nav-history" class="flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-purple-300 hover:bg-purple-200" aria-label="watch history">
+                    <button v-if="userData && $route.name === 'home'" id="btn-nav-history" @click="cycleSideBar('history')" class="flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-purple-300 hover:bg-purple-200" aria-label="watch history">
                         <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="24" width="24" class="fill-slate-900">
                             <path d="M0 0h24v24H0z" fill="none" />
                             <path fill-rule="evenodd" d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z" />
                         </svg>
                     </button>
-                    <button v-if="auth" id="btn-nav-settings" class=" hidden justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-red-300 hover:bg-red-200" aria-label="settings">
+                    <button v-if="userData" id="btn-nav-notifications" @click="cycleSideBar('notifications')" class="hidden justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-red-300 hover:bg-red-200" aria-label="notifications">
                         <svg xmlns="https://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="24" width="24" class="fill-slate-900">
                             <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
                         </svg>
                     </button>
+                    <RouterLink v-if="$route.name === 'history'" id="btn-nav-home" class="flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg bg-purple-300 hover:bg-purple-200" aria-label="home" to="/">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" height="24" width="24" class="stroke-slate-900">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                    </RouterLink>
                 </section>
 
                 <div class="toggle-switch shrink-0">
--- resources/js/stores/AuthStore.js
+++ resources/js/stores/AuthStore.js
@@ -2,27 +2,28 @@ import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore('Auth', () => {
-    var csrfToken = ref(document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') ?? '');
+    const csrfToken = ref(document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') ?? '');
 
-    const user = ref({
-        username: 'Aminushki',
-        email: 'test@tmu.ca',
-        avatar:'/storage/avatars/12345.jpg'
-    });
+    const userData = ref(null);
+    const isAuth = ref(null);
 
-    const pageTitle = ref('Full User History')
-    const auth = ref(true);
+    const user = ref(null);
+
+    const pageTitle = ref('');
     const darkMode = ref(true);
 
     const folders = ref([]);
     const videos = ref([]);
-    const records = ref([]);
+    const records = ref(['hah']);
 
     const stateDirectory = ref({id:7, name:'anime', folders: []})
     const stateFolder = ref({id:7, name:'ODDTAXI', videos: []})
 
-    const login = () => {
-        const remember_me = document.getElementById("remember_me");
+    const selectedSideBar = ref('');
+
+    const login = async (credentials) => {
+        // const remember_me = document.getElementById("remember_me");
+        const response = {};
         fetch(`/api/login`, {
             method: 'post',
             headers: {
@@ -30,29 +31,30 @@ export const useAuthStore = defineStore('Auth', () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'email': $('#email').val(),
-                'password': $('#password').val(), //'123456789!aB'
-                'remember_me': remember_me.checked
+                // 'email': $('#email').val(),
+                // 'password': $('#password').val(), //'123456789!aB'
+                // 'remember_me': remember_me.checked,
+                // '_token' : csrfToken
+                ...credentials
             })
         }).then((response) => 
             response.json()
         ).then((json) => {
             console.log(json);
-            if(json.success){
-                localStorage.setItem('auth-token', json.data.token)
-                toastr['info'](localStorage.getItem('auth-token'));
-                window.location.href = '/';
-            }
+            response['data'] = json?.data;
         }).catch((error) => {
             console.log(error);
+            response['error'] = error;
         });
+
+        return Promise.resolve(response);
     };
 
     const logout = () => {
         const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     
-        fetch(`/logout`, {
-            method: 'post',
+        fetch(`/api/logout`, {
+            method: 'delete',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
@@ -67,10 +69,55 @@ export const useAuthStore = defineStore('Auth', () => {
         }).catch((error) => {
             console.log(error);
         });
-    }
+    };
 
     const register = () => {
 
+    };
+
+    const auth = async () => {
+        /* 
+            Auth States:
+
+            1: Never logged in -> No token -> state is null
+            2: Logged in previously -> Token -> Token is invalid (ajax) -> State is false
+            3: Logged in previously -> Token -> Token is valid (ajax) -> State is true
+            4: State exists -> State is State (Logged in or out has already been checked)
+        
+        */
+        if(!localStorage.getItem('auth-token')){
+            console.log('no auth token');
+            return false;
+        }
+        
+        if(userData.value === null && !localStorage.getItem('auth-token')){
+            console.log('never logged in');
+            return false;
+        }
+
+        if(isAuth.value) return true; //checked
+        try {
+            const localToken = localStorage.getItem('auth-token');
+            const response = await fetch(`/api/auth`, {
+                method: 'get',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': localToken
+                }
+            })
+
+            const json = await response.json();
+            if(json){
+                isAuth.value = true;
+                console.log(json);
+                userData.value = json.data.user;
+                return true;
+            }
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 
     function toggleDarkMode(init = false){
@@ -78,31 +125,42 @@ export const useAuthStore = defineStore('Auth', () => {
         let lightModeToggle = document.getElementById('dark-mode-toggle') ?? {checked: true};
         if(!init || (init && darkMode.value === false)) darkMode.value = !lightModeToggle.checked;
 
-        let currElemClass = darkMode.value ? 'light-mode' : 'dark-mode';
-        let nextElemClass = darkMode.value ? 'dark-mode' : 'light-mode';
-        let currBtnClass = darkMode.value ? 'btn-light' : 'btn-dark';
-        let nextBtnClass = darkMode.value ? 'btn-dark' : 'btn-light';
+        // let currElemClass = darkMode.value ? 'light-mode' : 'dark-mode';
+        // let nextElemClass = darkMode.value ? 'dark-mode' : 'light-mode';
+        // let currBtnClass = darkMode.value ? 'btn-light' : 'btn-dark';
+        // let nextBtnClass = darkMode.value ? 'btn-dark' : 'btn-light';
 
         darkMode.value ? rootHTML.classList.add("dark", "tw-dark") : rootHTML.classList.remove("dark", "tw-dark");
 
-        let elems = document.querySelectorAll('.' + currElemClass);
-        let btns = document.querySelectorAll('.' + currBtnClass);
+        // let elems = document.querySelectorAll('.' + currElemClass);
+        // let btns = document.querySelectorAll('.' + currBtnClass);
 
-        for (let el of elems) {
-            el.classList.replace(currElemClass, nextElemClass);
-        }
+        // for (let el of elems) {
+            // el.classList.replace(currElemClass, nextElemClass);
+        // }
 
-        for (let btn of btns) {
-            btn.classList.replace(currBtnClass, nextBtnClass);
-        }
+        // for (let btn of btns) {
+            // btn.classList.replace(currBtnClass, nextBtnClass);
+        // }
         
-    }
+    };
+
+    function cycleSideBar(target = ''){
+        if(selectedSideBar.value === target) {
+            selectedSideBar.value = '';
+            document.getElementById('root').scrollIntoView({behavior: "smooth"});
+        }
+        else {
+            selectedSideBar.value = target;
+            // document.querySelector('#list-card').scrollIntoView({behavior: "smooth"});
+        }
+    };
 
     return {
-        user, auth, csrfToken, 
+        user, userData, auth, csrfToken, 
         login, logout, register, 
-        toggleDarkMode, 
-        pageTitle, darkMode, 
+        toggleDarkMode, cycleSideBar,
+        pageTitle, darkMode, selectedSideBar,
         folders, videos, records, 
         stateDirectory, stateFolder
     };
