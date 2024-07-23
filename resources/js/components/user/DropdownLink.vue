34f58dd787784c21823f5805ef11997e1ef05576
aminnausin <aminnausin@outlook.com>
2024-07-23 00:52:13 -0400

    dropdown modular component

---
+++ resources/js/components/user/DropdownLink.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+    const { linkData, selected } = defineProps(['linkData', 'selected'])
+</script>
+<template>
+    <RouterLink  
+        :class="{ 'font-bold': selected}"  :to="linkData.url"
+        class="rounded-t-md hover:bg-neutral-100 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"
+        role="menuitem"> {{ linkData.text }}
+    </RouterLink>
+</template>
