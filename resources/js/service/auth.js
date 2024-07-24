3b52306be584b68ef926707540cdfd1d44bc15f4
aminnausin <aminnausin@outlook.com>
2024-07-23 22:23:48 -0400

    move auth to a service and add an auth state checker to store

--- resources/js/service/auth.js
+++ resources/js/service/auth.js
@@ -2,51 +2,68 @@
 
 export const login = async (credentials) => {
     // const remember_me = document.getElementById("remember_me");
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await fetch(`/api/login`, {
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    // 'email': $('#email').val(),
-                    // 'password': $('#password').val(), //'123456789!aB'
-                    // 'remember_me': remember_me.checked,
-                    // '_token' : csrfToken
-                    ...credentials
-                })
+    try {
+        const response = await fetch(`/api/login`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // 'email': $('#email').val(),
+                // 'password': $('#password').val(), //'123456789!aB'
+                // 'remember_me': remember_me.checked,
+                // '_token' : csrfToken
+                ...credentials
             })
-            const json = await response.json()
-            resolve({response: json});
-        } catch (error) {
-            reject({error});
-        }
-    });
+        })
+        const json = await response.json()
+        return Promise.resolve({response: json});
+    } catch (error) {
+        return Promise.reject({error});
+    }
 };
 
-export const register = async () => {
-
+export const register = async (credentials) => {
+    try {
+        const response = await fetch(`/api/register`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                // 'name'
+                // 'email': $('#email').val(),
+                // 'password': $('#password').val(), //'123456789!aB'
+                // 'remember_me': remember_me.checked,
+                // '_token' : csrfToken
+                ...credentials
+            })
+        })
+        const json = await response.json()
+        return Promise.resolve({response: json});
+    } catch (error) {
+        return Promise.reject({error});
+    }
 }
 
 export const logout = async () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            const response = await fetch(`/api/logout`, {
-                method: 'delete',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': csrfToken
-                }
-            })
-            const json = await response.json()
-            resolve({response: json});
-        } catch (error) {
-            reject({error});
-        }
-    });
-    
+    try {
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        // const localToken = localStorage.getItem('auth-token');
+        const response = await fetch(`/api/logout`, {
+            method: 'delete',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-CSRF-TOKEN': csrfToken
+                // 'Authorization': "Bearer " + localToken,
+            }
+        })
+        const json = await response.json()
+        return Promise.resolve({response: json});
+    } catch (error) {
+        return Promise.reject({error});
+    }
 }
--- resources/js/stores/AuthStore.js
+++ resources/js/stores/AuthStore.js
@@ -1,5 +1,6 @@
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
+import { logout } from "../service/auth";
 
 export const useAuthStore = defineStore('Auth', () => {
     const csrfToken = ref(document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') ?? '');
@@ -21,60 +22,6 @@ export const useAuthStore = defineStore('Auth', () => {
 
     const selectedSideBar = ref('');
 
-    const login = async (credentials) => {
-        // const remember_me = document.getElementById("remember_me");
-        const response = {};
-        fetch(`/api/login`, {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                // 'email': $('#email').val(),
-                // 'password': $('#password').val(), //'123456789!aB'
-                // 'remember_me': remember_me.checked,
-                // '_token' : csrfToken
-                ...credentials
-            })
-        }).then((response) => 
-            response.json()
-        ).then((json) => {
-            console.log(json);
-            response['data'] = json?.data;
-        }).catch((error) => {
-            console.log(error);
-            response['error'] = error;
-        });
-
-        return Promise.resolve(response);
-    };
-
-    const logout = () => {
-        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-    
-        fetch(`/api/logout`, {
-            method: 'delete',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-CSRF-TOKEN': csrfToken
-            }
-        }).then(response => 
-            response.json()
-        ).then(json => {
-            console.log(json);
-            localStorage.clear('auth-token');
-            if(json.success == true) window.location.href = "/";
-        }).catch((error) => {
-            console.log(error);
-        });
-    };
-
-    const register = () => {
-
-    };
-
     const auth = async () => {
         /* 
             Auth States:
@@ -86,16 +33,17 @@ export const useAuthStore = defineStore('Auth', () => {
         
         */
         if(!localStorage.getItem('auth-token')){
-            console.log('no auth token');
+            // console.log('no auth token');
             return false;
         }
         
         if(userData.value === null && !localStorage.getItem('auth-token')){
-            console.log('never logged in');
+            // console.log('never logged in');
             return false;
         }
 
         if(isAuth.value) return true; //checked
+
         try {
             const localToken = localStorage.getItem('auth-token');
             const response = await fetch(`/api/auth`, {
@@ -103,19 +51,24 @@ export const useAuthStore = defineStore('Auth', () => {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': localToken
+                    'Authorization': "Bearer " + localToken,
                 }
             })
 
-            const json = await response.json();
-            if(json){
-                isAuth.value = true;
-                console.log(json);
-                userData.value = json.data.user;
-                return true;
+            if(response.status !== 200){
+                logout();
+                return false;
             }
+
+            const json = await response.json();
+
+            isAuth.value = true;
+            // console.log(json);
+            userData.value = json.data.user;
+            return true;
         } catch (error) {
             console.log(error);
+            logout();
             return false;
         }
     }
@@ -125,24 +78,7 @@ export const useAuthStore = defineStore('Auth', () => {
         let lightModeToggle = document.getElementById('dark-mode-toggle') ?? {checked: true};
         if(!init || (init && darkMode.value === false)) darkMode.value = !lightModeToggle.checked;
 
-        // let currElemClass = darkMode.value ? 'light-mode' : 'dark-mode';
-        // let nextElemClass = darkMode.value ? 'dark-mode' : 'light-mode';
-        // let currBtnClass = darkMode.value ? 'btn-light' : 'btn-dark';
-        // let nextBtnClass = darkMode.value ? 'btn-dark' : 'btn-light';
-
-        darkMode.value ? rootHTML.classList.add("dark", "tw-dark") : rootHTML.classList.remove("dark", "tw-dark");
-
-        // let elems = document.querySelectorAll('.' + currElemClass);
-        // let btns = document.querySelectorAll('.' + currBtnClass);
-
-        // for (let el of elems) {
-            // el.classList.replace(currElemClass, nextElemClass);
-        // }
-
-        // for (let btn of btns) {
-            // btn.classList.replace(currBtnClass, nextBtnClass);
-        // }
-        
+        darkMode.value ? rootHTML.classList.add("dark", "tw-dark") : rootHTML.classList.remove("dark", "tw-dark"); 
     };
 
     function cycleSideBar(target = ''){
@@ -152,13 +88,12 @@ export const useAuthStore = defineStore('Auth', () => {
         }
         else {
             selectedSideBar.value = target;
-            // document.querySelector('#list-card').scrollIntoView({behavior: "smooth"});
+            document.querySelector('#list-card').scrollIntoView({behavior: "smooth"});
         }
     };
 
     return {
         user, userData, auth, csrfToken, 
-        login, logout, register, 
         toggleDarkMode, cycleSideBar,
         pageTitle, darkMode, selectedSideBar,
         folders, videos, records, 
