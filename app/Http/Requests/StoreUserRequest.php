33b471b1af15e93015cbd85b5d7b4070205a175a
aminnausin <aminnausin@outlook.com>
2024-07-23 21:45:51 -0400

    Require unique username to register

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -23,7 +23,7 @@ class StoreUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()]
         ];
