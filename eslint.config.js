b80b6021945dc2e6aea9d99615b0ee6a3453bfd9
aminnausin <aminnausin@outlook.com>
2024-07-24 00:50:33 -0400

    lint directory update

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
+    { 
+        files: ["resources/*.{js,mjs,cjs,vue}"],
+    },
+    pluginJs.configs.recommended,
+    ...pluginVue.configs["flat/essential"],
 ];
