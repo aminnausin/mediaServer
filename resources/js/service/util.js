b5ead0e43c5ef3ccad52b2d20df5f1eb01d24ff8
aminnausin <aminnausin@outlook.com>
2024-07-24 18:11:24 -0400

    add Timespan util function

--- resources/js/service/util.js
+++ resources/js/service/util.js
@@ -3,3 +3,17 @@ export function toTitleCase(str) {
         return match.toUpperCase();
     });
 }
+
+export function toTimeSpan(rawDate) {
+    const rawAge = Date.now() - rawDate.getTime();
+        
+    const weeks = Math.round(rawAge / (1000 * 3600 * 24 * 7));
+    const days = Math.round(rawAge / (1000 * 3600 * 24));
+    const hours = Math.round(rawAge / (1000 * 3600));
+    const minutes = Math.round(rawAge / (1000 * 60));
+    const seconds = Math.round(rawAge / (1000));
+        
+    const timeSpan = weeks > 0 ? `${weeks} week${weeks > 1 ? 's' : ''} ago` : days > 0 ? `${days} day${days > 1 ? 's' : ''} ago` : hours > 0 ? `${hours} hour${hours > 1 ? 's' : ''} ago` : minutes > 0 ? `${minutes}m ago` : `${seconds}s ago`
+
+    return timeSpan;
+}
