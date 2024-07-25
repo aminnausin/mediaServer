0c7844d77d3fad8efb3f466c15681536c8823a2d
aminnausin <aminnausin@outlook.com>
2024-07-25 13:18:29 -0400

    New labeled text input (not final) and remove blue text from darktoggle

---
+++ resources/js/components/inputs/LabelledTextInput.vue
@@ -0,0 +1,11 @@
+<script setup>
+const model = defineModel();
+const { text } = defineProps(['text'])
+</script>
+<template>
+    <div class="flex justify-start items-center gap-2">
+        <label class="text-neutral-950 dark:text-white text-lg" :for="text">{{ text }}:</label>
+        <input type="text" v-model="model"
+            class="p-2 text-neutral-950 dark:text-white bg-input-100 border-input-200 shadow-sm leading-none dark:bg-input-800 dark:border-indigo-800 rounded-lg">
+    </div>
+</template>
--- resources/js/components/inputs/LightModeToggle.vue
+++ resources/js/components/inputs/LightModeToggle.vue
@@ -9,7 +9,7 @@
 <template>
     <section class="toggle-switch shrink-0 flex flex-col">
         <label class="switch-label">
-            <input type="checkbox" class="checkbox invisible" id="light-mode-toggle" v-model="lightMode" v-bind:checked="lightMode" >
+            <input type="checkbox" class="checkbox invisible text-white" id="light-mode-toggle" v-model="lightMode" v-bind:checked="lightMode" >
             <span class="slider"></span>
         </label>
     </section>
