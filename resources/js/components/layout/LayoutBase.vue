18238a4a5ab6fdbc9d52ad1f5da2d220f18b315e
aminnausin <aminnausin@outlook.com>
2024-07-27 15:59:19 -0400

    Center main card correctly

--- resources/js/components/layout/LayoutBase.vue
+++ resources/js/components/layout/LayoutBase.vue
@@ -9,7 +9,7 @@ const { selectedSideBar } = storeToRefs(appStore);
 
 <template>
     <main class="p-6 flex gap-6 flex-wrap lg:flex-nowrap snap-y">
-        <section id="left-card" class="w-full lg:w-1/6 lg:max-w-72 sm:min-w-32 hidden lg:block">
+        <section id="left-card" class="w-full p-3 pb-6 rounded-2xl lg:w-1/6 lg:max-w-72 sm:min-w-32 shrink-0 space-y-2 scroll-mt-6 hidden lg:block">
         </section>
         <section id="content-card"
             class="bg-primary-900 dark:bg-primary-dark-900 dark:text-[#e2e0e2] shadow-xl p-6 pt-3 rounded-2xl w-full flex flex-col gap-3 overflow-x-hidden">
