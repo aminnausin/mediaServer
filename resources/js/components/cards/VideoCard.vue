59e3023d6d8ed723b689dfd9d23da0f6921db45b
aminnausin <aminnausin@outlook.com>
2024-07-28 18:41:38 -0400

    Add new columns to videos

--- resources/js/components/cards/VideoCard.vue
+++ resources/js/components/cards/VideoCard.vue
@@ -17,11 +17,11 @@ const handlePlay = () => {
         class="shadow vid-row w-full rounded-md dark:bg-neutral-800 dark:hover:bg-indigo-900 bg-gray-100 hover:bg-violet-300 odd:bg-violet-100 hover:odd:bg-violet-300 dark:odd:bg-zinc-700 dark:hover:odd:bg-indigo-900 p-3 flex ring-inset flex-wrap flex-col sm:flex-row sm:flex-nowrap"
         :data-id="props.video.id" :data-path="`../${props.video.attributes.path}`" @click="handlePlay">
         <td class="flex gap-12 w-full">
-            <p class="vid-row-title w-full xl:min-w-48 sm:max-w-1/2 sm:w-auto flex justify-between items-center gap-12 sm:gap-0"><span class="sm:hidden">Title: </span><span class="line-clamp-1">{{ props.video.attributes.name }}</span></p>
-            <p class="vid-row-duration truncate hidden sm:block">20m 30s</p>
+            <p class="vid-row-title w-full xl:min-w-48 sm:max-w-1/2 sm:w-auto flex justify-between items-center gap-12 sm:gap-0"><span class="sm:hidden">Title: </span><span class="line-clamp-1">{{ props.video.attributes.title ?? props.video.attributes.name }}</span></p>
+            <p class="vid-row-duration truncate hidden sm:block">{{ props.video.attributes.duration ?? 'N/A' }}</p>
         </td>
         <td class="flex gap-12 w-full sm:justify-end">
-            <p class="vid-row-title truncate hidden sm:block">20 Views</p>
+            <p class="vid-row-title truncate hidden sm:block">{{ props.video.attributes.view_count ?? 'No Views' }}</p>
             <p class="vid-row-date w-full xl:min-w-36 sm:max-w-1/3 sm:w-auto flex justify-between items-center gap-12 sm:gap-0"><span class="sm:hidden">Date: </span><span class="line-clamp-1">{{ toFormattedDate(new Date(props.video.attributes.date + ' GMT')) }}</span></p>
         </td>
     </tr>
--- resources/js/components/video/VideoPlayer.vue
+++ resources/js/components/video/VideoPlayer.vue
@@ -3,6 +3,7 @@ import { storeToRefs } from 'pinia';
 import { useContentStore } from '../../stores/ContentStore';
 import { computed, ref, watch } from 'vue';
 
+// Heatmap can be composable
 const heatMapData = ref([{ x: 25, y: 60 }, { x: 154, y: 48 }, { x: 266, y: 12 }, { x: 585, y: 18 },{ x: 799, y: 16 }, { x: 1000, y: 100 }]);
 const heatMap = computed(() => {
     const start = 'M 0.0,100.0 ';
