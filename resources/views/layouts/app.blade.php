4b8e15085e8f7e1bb67dd73adc6b183612361c9f
aminnausin <aminnausin@outlook.com>
2024-07-28 02:52:56 -0400

    Jquery for toastr

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
 
         <!-- Jquery -->
-        <!-- <script src="https://code.jquery.com/jquery-3.7.1.js"></script> -->
+        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 
         <!-- DataTables -->
         <!-- <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script> -->
