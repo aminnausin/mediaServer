92c979da9794d26471a5bdce05bb6ba938f082d5
aminnausin <aminnausin@outlook.com>
2024-08-02 00:17:08 -0400

    Tests page

--- resources/js/views/ProfileView.vue
+++ resources/js/views/ProfileView.vue
@@ -1,8 +1,23 @@
 <script setup>
 import LayoutBase from '../components/layout/LayoutBase.vue';
 import useToast from '../composables/useToast';
+import ButtonText from '../components/inputs/ButtonText.vue'
+
+import { onMounted } from 'vue';
+import { useAppStore } from '../stores/AppStore';
+import { storeToRefs } from 'pinia';
+
 const emit = defineEmits(['toast-show']);
 const toast = useToast({emit});
+
+const appStore = useAppStore();
+
+const { pageTitle, selectedSideBar } = storeToRefs(appStore);
+
+onMounted(() => {
+    pageTitle.value = "Tests";
+    selectedSideBar.value = '';
+})
 </script>
 
 <template>
@@ -12,12 +27,13 @@ const toast = useToast({emit});
                 <div class="relative w-auto h-auto">
                     <div class="relative space-y-5">
                         <div class="relative">
-                            <p class="mb-2 text-xs font-medium text-center text-gray-500 sm:text-left">Types</p>
+                            <p class="mb-2 text-xs font-medium text-center text-gray-500 sm:text-left">Tests</p>
                             <div class="relative flex flex-col px-10 space-y-2 sm:space-x-5 sm:space-y-0 sm:flex-row sm:px-0">
-                                <button class="inline-flex items-center justify-center flex-shrink-0 px-3 py-1 text-xs font-medium transition-colors border rounded-md h-9 hover:bg-gray-50 active:bg-white focus:bg-white focus:outline-none"
-                                    @click="toast.popToast();">
-                                    Default
-                                </button>
+                                <ButtonText  @click="toast.popToast();">
+                                    <template #text>
+                                        Test Notification
+                                    </template>
+                                </ButtonText>
                                 <!-- <button class="inline-flex items-center justify-center flex-shrink-0 px-3 py-1 text-xs font-medium transition-colors border rounded-md h-9 hover:bg-gray-50 active:bg-white focus:bg-white focus:outline-none"
                                     @click="$emit('setPage', null)">
                                     Default
