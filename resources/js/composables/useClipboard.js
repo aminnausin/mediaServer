c209b69ec567cbd382dc31fdbdda2745efdac47c
aminnausin <aminnausin@outlook.com>
2024-08-02 18:34:46 -0400

    clipboard error

--- resources/js/composables/useClipboard.js
+++ resources/js/composables/useClipboard.js
@@ -1,16 +1,24 @@
 import { reactive } from "vue";
+import { useToast } from '../composables/useToast';
 
 export default function useClipboard(copyText) {
+
+    const toast = useToast();
     return reactive({
         copyText,
         copyNotification: false,
         copyToClipboard() {
-            navigator.clipboard.writeText(this.copyText);
-            this.copyNotification = true;
-            let that = this;
-            setTimeout(function () {
-                that.copyNotification = false;
-            }, 3000);
+            try {
+                navigator.clipboard.writeText(this.copyText);
+                this.copyNotification = true;
+                let that = this;
+                setTimeout(function () {
+                    that.copyNotification = false;
+                }, 3000);
+            } catch (error) {
+                console.log(error);
+                toast.add({ type: 'danger', title:'Error', description:'Unable to copy. Network is not secure.', life: 3000});
+            }
         }
     });
 }
