c057ff8e127c2e997a44ef86256524877a989f8f
aminnausin <aminnausin@outlook.com>
2024-08-02 18:44:20 -0400

    delete old files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,2 @@
 <x-app-layout>
-    @auth
-    <script>var user = true;</script>
-    @else
-    <script>var user = false;</script>
-    @endauth
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,37 +5,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="A self hosted web media server to keep everything in one place.">
-        <!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Bootstrap -->
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css" rel="stylesheet"> -->
-        <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
-
-        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
-
+        <title>{{ config('app.name', 'Media Server') }}</title>
         <!-- Jquery -->
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-
-        <!-- DataTables -->
-        <!-- <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script> -->
-        <!-- <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script> -->
-
-        <!-- <link href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet" crossorigin="anonymous"> -->
-        <!-- <link href="https://cdn.datatables.net/1.13.7/css/dataTables.tailwindcss.min.css" rel="stylesheet"> -->
-        <!-- <link href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css" rel="stylesheet"> -->
-        <!-- toastr -->
         <link href="{{ URL::asset('css/toastr.min.css') }}" rel="stylesheet">
         <script src="{{ URL::asset('js/toastr.min.js') }}"></script>
 
         <!-- local -->
-
-        <!-- <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet"> -->
-        <!-- <script src="{{ URL::asset('js/auth.js') }}"></script> -->
         @vite('resources/css/app.css')
-
-        
         <script>
             toastr.options = {
                 "closeButton": true,
