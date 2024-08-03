3265f0ca5d01cd04ee8cde49400aa6f3d2cff06c
aminnausin <aminnausin@outlook.com>
2024-08-03 15:23:44 -0400

    make folder and record cards consistant

--- resources/js/components/cards/VideoCard.vue
+++ resources/js/components/cards/VideoCard.vue
@@ -21,7 +21,7 @@ watch(props, handlePropsUpdate, {immediate: true});
 
 <template>
     <div :class="{ 'ring-violet-600/70 ring-[0.125rem]': (props.currentID === props.video?.id) }"
-        class="flex flex-wrap flex-col sm:flex-row sm:flex-nowrap gap-x-8 gap-y-4 p-3 w-full shadow rounded-md ring-inset dark:bg-neutral-800 dark:hover:bg-indigo-900 bg-gray-100 hover:bg-violet-300 odd:bg-violet-100 hover:odd:bg-violet-300 dark:odd:bg-zinc-700 dark:hover:odd:bg-indigo-900"
+        class="flex flex-wrap flex-col sm:flex-row sm:flex-nowrap gap-x-8 gap-y-4 p-3 w-full shadow rounded-md ring-inset dark:bg-primary-dark-800/70 dark:hover:bg-violet-700/70 bg-gray-100 hover:bg-violet-400/30 odd:bg-violet-100 dark:odd:bg-primary-dark-600"
         :data-id="props.video?.id" :data-path="`../${props.video?.attributes.path}`" @click="handlePlay">
         <section class="flex justify-between gap-4 w-full items-start sm:items-center">
             <h3 class="w-full line-clamp-2 sm:line-clamp-1">
@@ -39,14 +39,5 @@ watch(props, handlePropsUpdate, {immediate: true});
                 {{ toFormattedDate(new Date(props.video?.attributes.date + ' GMT')) }}
             </h3>
         </section>
-        
-        <!-- <td class="flex gap-12 w-full">
-            <p class="vid-row-title w-full xl:min-w-48 sm:max-w-1/2 sm:w-auto flex justify-between items-center gap-12 sm:gap-0"><span class="sm:hidden">Title: </span><span class="line-clamp-1">{{ metaData?.fields?.title }}</span></p>
-            <p class="vid-row-duration truncate hidden sm:block">{{ metaData?.fields?.duration }}</p>
-        </td>
-        <td class="flex gap-12 w-full sm:justify-end">
-            <p class="vid-row-title truncate hidden sm:block">{{ metaData?.fields?.views }}</p>
-            <p class="vid-row-date w-full xl:min-w-36 sm:max-w-1/3 sm:w-auto flex justify-between items-center gap-12 sm:gap-0"><span class="sm:hidden">Date: </span><span class="line-clamp-1">{{ toFormattedDate(new Date(props.video?.attributes.date + ' GMT')) }}</span></p>
-        </td> -->
     </div>
 </template>
