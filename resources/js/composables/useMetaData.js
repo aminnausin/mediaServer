e1dd6fe9cbdb0761ad8b759c9da5bc72a5db9dab
aminnausin <aminnausin@outlook.com>
2024-08-14 22:27:19 -0400

    encode share url to replace spaces with url spaces

--- resources/js/composables/useMetaData.js
+++ resources/js/composables/useMetaData.js
@@ -13,14 +13,14 @@ export default function useMetaData(data) {
             duration: toFormattedDuration(data?.duration) ?? 'N/A',
             views: data?.view_count ? `${data?.view_count} View${data?.view_count !== 1 ? 's' : ''}` : '0 Views',
             description: data.description ?? '',
-            url: (data?.skipBaseURL ? '' : document.location.origin) + route.path + `?video=${data.id}`,
+            url: encodeURI((data?.skipBaseURL ? '' : document.location.origin) + route.path + `?video=${data.id}`),
         },
         updateData(props){
             this.fields.title = props?.title ?? props?.name,
             this.fields.duration = toFormattedDuration(props?.duration) ?? 'N/A',
             this.fields.views = props?.view_count ? `${props?.view_count} View${props?.view_count !== 1 ? 's' : ''}` : '0 Views',
             this.fields.description = props?.description ?? '',
-            this.fields.url = (props?.skipBaseURL ? '' : document.location.origin) + route.path + `?video=${props.id}`
+            this.fields.url = encodeURI((props?.skipBaseURL ? '' : document.location.origin) + route.path + `?video=${props.id}`)
         }
     });
 }
