a330573dfe8172957c5e41f2e2f6ec7c88cd91eb
aminnausin <aminnausin@outlook.com>
2024-08-18 17:31:50 -0400

    update edit video and add exit folder

--- resources/js/components/video/VideoInfoPanel.vue
+++ resources/js/components/video/VideoInfoPanel.vue
@@ -4,18 +4,22 @@ import useMetaData from '../../composables/useMetaData';
 import ModalBase from '../pinesUI/ModalBase.vue';
 import useModal from '../../composables/useModal';
 import EditVideo from '../forms/EditVideo.vue';
+import EditFolder from '../forms/EditFolder.vue';
 
 import CircumShare1 from '~icons/circum/share-1';
 
 import { watch } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useContentStore } from '../../stores/ContentStore';
+import { useAuthStore } from '../../stores/AuthStore';
 import ButtonIcon from '../inputs/ButtonIcon.vue';
 import ButtonText from '../inputs/ButtonText.vue';
 
 
 const ContentStore = useContentStore();
+const AuthStore = useAuthStore();
 const { stateVideo } = storeToRefs(ContentStore);
+const { userData } = storeToRefs(AuthStore);
 const { updateVideoData } = ContentStore;
 
 
@@ -38,8 +42,9 @@ const defaultDescription = `After defeating the
                     seeing Fern is already remarkably skilled despite her youth.`;
 
 const metaData = useMetaData({ ...stateVideo.value.attributes, id: stateVideo.value.id }, stateVideo.value);
-const editModal = useModal({ title: 'Edit Video Details', submitText: 'Submit Details' });
-const shareModal = useModal({ title: 'Share Video' });
+const editFolderModal = useModal({ title: 'Edit Folder Details', submitText: 'Submit Details' });
+const editVideoModal = useModal({ title: 'Edit Video Details', submitText: 'Submit Details' });
+const shareVideoModal = useModal({ title: 'Share Video' });
 
 const handlePropsUpdate = () => {
     metaData.updateData({ ...stateVideo.value.attributes, id: stateVideo.value.id });
@@ -50,7 +55,15 @@ const handleVideoDetailsUpdate = (res) => {
         stateVideo.value = { index: stateVideo.value.index, ...res.data }
         updateVideoData({ index: stateVideo.value.index, ...res.data }, stateVideo.value.index);
     }
-    editModal.toggleModal(false);
+    editVideoModal.toggleModal(false);
+}
+
+const handleSeriesUpdate = (res) => {
+    if (res?.data) {
+        stateVideo.value = { index: stateVideo.value.index, ...res.data }
+        updateVideoData({ index: stateVideo.value.index, ...res.data }, stateVideo.value.index);
+    }
+    editVideoModal.toggleModal(false);
 }
 
 watch(() => stateVideo.value, handlePropsUpdate, { immediate: true, deep: true });
@@ -62,7 +75,9 @@ watch(() => stateVideo.value, handlePropsUpdate, { immediate: true, deep: true }
         <div id="mp4-description" class="flex items-center gap-4 w-full md:w-2/3 ">
             <img id="folder-thumbnail" class="h-28 object-contain rounded-md shadow-md"
                 :src="stateVideo?.attributes?.thumbnail?.url ?? 'https://m.media-amazon.com/images/M/MV5BMjVjZGU5ZTktYTZiNC00N2Q1LThiZjMtMDVmZDljN2I3ZWIwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg'"
-                alt="Folder Cover Art">
+                alt="Folder Cover Art"
+                @click="() => {if(userData) editFolderModal.toggleModal()}"
+                title="Edit Folder Details">
             <div class="h-full flex flex-col gap-2">
                 <div id="mp4-title" class="text-xl font-medium line capitalize">
                     {{ metaData?.fields.title ?? '[Video Name]' }}
@@ -76,14 +91,14 @@ watch(() => stateVideo.value, handlePropsUpdate, { immediate: true, deep: true }
             class="container flex sm:w-auto sm:flex-col justify-between lg:min-w-32 items-center sm:items-end gap-3 flex-wrap flex-1 w-full"
             role="group">
             <section class="flex gap-2 justify-end">
-                <ButtonText aria-label="edit details" title="Edit Video Details" @click="editModal.toggleModal()">
+                <ButtonText v-if="userData" aria-label="edit details" title="Edit Video Details" @click="editVideoModal.toggleModal()">
                     <template #text>
                         <p class="text-nowrap">
                             Edit Details
                         </p>
                     </template>
                 </ButtonText>
-                <ButtonIcon aria-label="share" title="Share Video" @click="shareModal.toggleModal()">
+                <ButtonIcon aria-label="share" title="Share Video" @click="shareVideoModal.toggleModal()">
                     <template #icon>
                         <CircumShare1 height="24" width="24" />
                     </template>
@@ -100,21 +115,28 @@ watch(() => stateVideo.value, handlePropsUpdate, { immediate: true, deep: true }
             </section>
         </div>
     </div>
-    <ModalBase :modalData="editModal" :useControls="false">
+    <ModalBase :modalData="editFolderModal" :useControls="false">
+        <template #content>
+            <div class="pt-3">
+                <EditFolder :video="stateVideo" @handleFinish="handleSeriesUpdate" />
+            </div>
+        </template>
+    </ModalBase>
+    <ModalBase :modalData="editVideoModal" :useControls="false">
         <template #content>
             <div class="pt-3">
                 <EditVideo :video="stateVideo" @handleFinish="handleVideoDetailsUpdate" />
             </div>
         </template>
     </ModalBase>
-    <ModalBase :modalData="shareModal">
+    <ModalBase :modalData="shareVideoModal">
         <template #content>
             <div class="py-3">
                 Copy link to clipboard to share it.
             </div>
         </template>
         <template #controls>
-            <ButtonClipboard :text="metaData.fields.url" />
+            <ButtonClipboard :text="metaData.fields.url" tabindex="1"/>
         </template>
     </ModalBase>
 </template>
