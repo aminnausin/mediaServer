bcb9868a21b37141be14633fad17dd324901470f
aminnausin <aminnausin@outlook.com>
2024-09-13 21:34:31 -0400

    metadata and series requests

--- app/Http/Requests/MetadataUpdateRequest.php
+++ app/Http/Requests/MetadataUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class MetadataUpdateRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class MetadataUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::check();
     }
 
     /**
