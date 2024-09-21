57db90de2919d491c8dce107ca74ea5d5a7ba0a4
aminnausin <aminnausin@outlook.com>
2024-09-21 12:25:39 -0400

    prettier

--- resources/js/views/SettingsView.vue
+++ resources/js/views/SettingsView.vue
@@ -1,37 +1,34 @@
 <script setup>
-import LayoutBase from '../layouts/LayoutBase.vue';
-import FormInputLabel from '../components/labels/FormInputLabel.vue'
-
 import { storeToRefs } from 'pinia';
 import { useAppStore } from '../stores/AppStore';
 import { onMounted } from 'vue';
-import ToggleBase from '../components/inputs/ToggleBase.vue';
-// import { useToast } from '../composables/useToast';
 
-// const toast = useToast();
+import FormInputLabel from '../components/labels/FormInputLabel.vue';
+import ToggleBase from '../components/inputs/ToggleBase.vue';
+import LayoutBase from '../layouts/LayoutBase.vue';
 
 const appStore = useAppStore();
 
 const { pageTitle, ambientMode } = storeToRefs(appStore);
 
 onMounted(() => {
-    pageTitle.value = "Settings";
-})
+	pageTitle.value = 'Settings';
+});
 </script>
 
 <template>
-    <LayoutBase>
-        <template v-slot:content>
-            <section id="content-settings" class="space-y-2 cursor-pointer min-h-[80vh]">
-                <FormInputLabel :field="{name: 'ToggleAmbientMode', text: 'Ambient Mode'}"/>
-                <div class="flex items-center gap-2">
-                    <ToggleBase v-model="ambientMode"/>
-                    <p>{{ ambientMode ? 'On' : 'Off' }}</p>
-                </div>
-            </section>
-        </template>
-        <template v-slot:sidebar>
-            <!-- <Settings /> -->
-        </template>
-    </LayoutBase>
+	<LayoutBase>
+		<template v-slot:content>
+			<section id="content-settings" class="space-y-2 cursor-pointer min-h-[80vh]">
+				<FormInputLabel :field="{ name: 'ToggleAmbientMode', text: 'Ambient Mode' }" />
+				<div class="flex items-center gap-2">
+					<ToggleBase v-model="ambientMode" />
+					<p>{{ ambientMode ? 'On' : 'Off' }}</p>
+				</div>
+			</section>
+		</template>
+		<template v-slot:sidebar>
+			<!-- <Settings /> -->
+		</template>
+	</LayoutBase>
 </template>
