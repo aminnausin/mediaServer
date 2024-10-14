b59f442348b834ec65e6ba2afc1638a96bf201d3
aminnausin <aminnausin@outlook.com>
2024-10-13 21:36:31 -0400

    fix lint idk

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 import pluginJs from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
 
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
 export default [
+    includeIgnoreFile(gitignorePath),
     {
-        files: ['resources/*.{js,mjs,cjs,vue}'],
+        files: ['resources/*.{js,mjs,cjs,vue,ts}'],
     },
     pluginJs.configs.recommended,
     ...pluginVue.configs['flat/essential'],
