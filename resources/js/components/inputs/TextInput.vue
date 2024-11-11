a3c594973779ec4b0d213928c5562aecebb7fee0
aminnausin <aminnausin@outlook.com>
2024-11-10 22:46:49 -0500

    generic text inpuit

---
+++ resources/js/components/inputs/TextInput.vue
@@ -0,0 +1,19 @@
+<script setup>
+const model = defineModel();
+const { placeholder, id } = defineProps(['placeholder', 'id']);
+</script>
+<template>
+    <input
+        type="text"
+        v-model="model"
+        :name="id"
+        :id="id"
+        :placeholder="placeholder"
+        :class="`h-10 p-2 shadow-sm rounded-md w-full text-sm text-ellipsis whitespace-nowrap line-clamp-1
+            border-none outline-none
+            disabled:cursor-not-allowed disabled:opacity-50
+            text-gray-900 dark:text-neutral-100 bg-white dark:bg-primary-dark-800 placeholder:text-neutral-400
+            ring-inset focus:ring-inset ring-[1px] ring-neutral-200 dark:ring-neutral-700
+            hocus:ring-[0.125rem] focus:ring-indigo-400 dark:focus:ring-indigo-500 hover:ring-violet-400 hover:dark:ring-violet-700`"
+    />
+</template>
