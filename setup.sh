2da254e1b1227c6eafce83d201d9abc3c1595f13
aminnausin <aminnausin@outlook.com>
2025-01-05 00:41:19 -0500

    bad setup file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 git pull
 
 npm i
 composer install
 
-php artisan migrate
+cp .env.example .env
+
 php artisan key:generate
+
+npm run build
+
+echo "Setup a database connection in the .env file and then run php artisan migrate"
