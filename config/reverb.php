a808e3a74ba9a996c79dc1689b234820f2016605
aminnausin <aminnausin@outlook.com>
2025-01-11 14:01:50 -0500

    reverb ports?

--- config/reverb.php
+++ config/reverb.php
@@ -33,7 +33,9 @@ return [
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'verify_peer' => env('APP_ENV') !== 'local'
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
