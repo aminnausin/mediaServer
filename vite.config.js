03df7d6066dffecf6808eb68f40fd0fe1c51d463
aminnausin <aminnausin@outlook.com>
2025-01-13 21:57:37 -0500

    vite mode ?

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,40 @@
 // import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 import viteCompression from 'vite-plugin-compression';
 import laravel from 'laravel-vite-plugin';
 import Icons from 'unplugin-icons/vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    mode: import.meta.env.APP_ENV === 'local' ? 'development' : 'production',
-    plugins: [
-        viteCompression(),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        mode: env.APP_ENV === 'local' ? 'development' : 'production',
+        plugins: [
+            viteCompression(),
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.ts'],
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+            Icons({
+                // experimental
+                autoInstall: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                vue: 'vue/dist/vue.esm-bundler.js',
+                // '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
-        }),
-        Icons({
-            // experimental
-            autoInstall: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            // '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-    },
+    };
 });
