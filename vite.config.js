6ef1b858d32e048e9f5ae5b616a1a60948807ce1
aminnausin <aminnausin@outlook.com>
2025-01-13 21:52:13 -0500

    compress code

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 // import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
+import viteCompression from 'vite-plugin-compression';
 import laravel from 'laravel-vite-plugin';
 import Icons from 'unplugin-icons/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        viteCompression(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
