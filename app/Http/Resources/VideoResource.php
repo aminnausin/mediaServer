742ec1c9bd77899d401a96a2a8f2d8099c611cf6
aminnausin <aminnausin@outlook.com>
2025-02-01 20:44:57 -0500

    fix video resource duplicate database queries

--- app/Http/Resources/VideoResource.php
+++ app/Http/Resources/VideoResource.php
@@ -6,46 +6,45 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class VideoResource extends JsonResource {
+    protected static $metadataCache = [];
+
+    protected static $videoTagCache = [];
+
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array {
-        $metadata = $this->metadata;
+
+        $metadata = null;
+
+        $metadataId = $this->metadata_id;
+
+        if (isset(self::$metadataCache[$metadataId])) {
+            $metadata = self::$metadataCache[$metadataId];
+        } else {
+            $metadata = $this->metadata;
+            self::$metadataCache[$metadataId] = $metadata;
+        }
 
         return [
             'id' => (string) $this->id,
-            // 'path' => $this->path,
-            // 'name' => $this->name,
-            // 'date' => $this->date,
-
-            // 'folder_id' => (string)$this->folder->id,
-            // 'metadata' => new MetadataResource($metadata),
-
-            // 'attributes' => [
             'name' => $this->name,
             'path' => $this->path,
             'date' => $this->date,
-            // 'date_raw' => $this->date_raw,
-            'title' => ($metadata ? $metadata->title : $this->title) ?? $this->name,
-            'description' => $metadata ? $metadata->description : $this->description, // ?? $this->folder->series->description
-            'duration' => $metadata ? $metadata->duration : $this->duration,
-            'episode' => $metadata ? $metadata->episode : $this->episode,
-            'season' => $metadata ? $metadata->season : $this->season,
-            'view_count' => $metadata ? $metadata->view_count : $this->view_count,
-            'file_size' => $metadata ? $metadata->file_size : null,
-            // 'tags' => $metadata->tags ?? '',
-            'video_tags' => $metadata ? VideoTagResource::collection($metadata->videotags) : [],
-            'date_released' => $metadata ? $metadata->date_released : null,
-            'date_updated' => $metadata ? $metadata->updated_at : null,
-            'date_uploaded' => $metadata ? $metadata->date_uploaded : null,
-            // ],
-            // 'relationships' => [
-            // 'folder_id' => (string)$this->folder->id,
+            'title' => $metadata?->title ?: $this->title ?: $this->name,
+            'description' => $metadata?->description ?: $this->description, // ?: $this->folder->series->description
+            'duration' => $metadata?->duration ?: $this->duration,
+            'episode' => $metadata?->episode ?: $this->episode,
+            'season' => $metadata?->season ?: $this->season,
+            'view_count' => $metadata?->view_count ?: $this->view_count,
+            'file_size' => $metadata?->file_size ?: null,
+            'video_tags' => VideoTagResource::collection($metadata?->videotags ?: []),
+            'date_released' => $metadata?->date_released ?: null,
+            'date_updated' => $metadata?->updated_at ?: null,
+            'date_uploaded' => $metadata?->date_uploaded ?: null,
             'metadata' => $metadata,
-            // 'editor' => $metadata ? $metadata->editor : null
-            // ]
         ];
     }
 }
