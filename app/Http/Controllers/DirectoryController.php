aa789086f73931c5bf95184691623cf73269ad6d
aminnausin <aminnausin@outlook.com>
2025-02-02 13:42:51 -0500

    pint

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Enums\TaskStatus;
 use App\Events\TaskEnded;
 use App\Http\Resources\FolderResource;
-use App\Http\Resources\SeriesResource;
 use App\Http\Resources\VideoResource;
 use App\Jobs\CleanFolderPaths;
 use App\Jobs\CleanVideoPaths;
