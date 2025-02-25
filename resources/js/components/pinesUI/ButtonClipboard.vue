8c9a81a1dee53b0eb3432b0665bc3240c3926e56
aminnausin <aminnausin@outlook.com>
2025-02-25 16:08:58 -0500

    fix: remove alpine references in two components (1 unused)

--- resources/js/components/pinesUI/ButtonClipboard.vue
+++ resources/js/components/pinesUI/ButtonClipboard.vue
@@ -68,7 +68,7 @@ const copyToClipboard = async () => {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        x-cloak
+                        v-cloak
                     >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                     </svg>
