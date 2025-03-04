397a4b16fc09d8f27375326a23f3697dd0ec7f51
aminnausin <aminnausin@outlook.com>
2025-03-04 15:39:12 -0500

    fix: hover card max height

--- resources/js/components/cards/HoverCard.vue
+++ resources/js/components/cards/HoverCard.vue
@@ -73,7 +73,7 @@ watch(
 <template>
     <div class="" @mouseover="hoverCardEnter" @mouseleave="hoverCardLeave">
         <slot name="trigger">
-            <a href="#_" class="hover:underline">@thedevdojo</a>
+            <a href="#_" class="hover:underline"></a>
         </slot>
         <Teleport to="body" v-if="init">
             <Transition enter-from-class="opacity-0" enter-to-class="opacity-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
@@ -82,14 +82,14 @@ watch(
                     @mouseleave="hoverCardLeave"
                     v-show="hoverCardHovered"
                     v-cloak
-                    :class="`${positionClasses ?? ''} z-30 flex absolute overflow-auto transition-opacity ease-in-out duration-200 md:max-w-xl xl:max-w-3xl text-sm p-3 h-fit bg-white dark:odd:bg-primary-dark-600/70 dark:bg-neutral-800/70 backdrop-blur-lg border dark:border-none rounded-md shadow-md border-neutral-200/70 gap-2 items-center`"
+                    :class="`${positionClasses ?? ''} z-30 flex absolute overflow-auto transition-opacity ease-in-out duration-200 md:max-w-xl xl:max-w-3xl text-sm p-3 h-fit scrollbar-minimal bg-white dark:odd:bg-primary-dark-600/70 dark:bg-neutral-800/70 backdrop-blur-lg border dark:border-none rounded-md shadow-md border-neutral-200/70 gap-2 items-center`"
                     :style="tooltipStyles"
                 >
                     <slot name="icon">
                         <ProiconsCommentExclamation v-if="!iconHidden" class="h-5 w-5 mb-auto shrink-0" />
                     </slot>
                     <slot name="content">
-                        <p class="text-pretty h-fit w-full break-words whitespace-pre-wrap">
+                        <p class="text-pretty h-fit max-h-[50vh] w-full break-words whitespace-pre-wrap">
                             {{ content }}
                         </p>
                     </slot>
