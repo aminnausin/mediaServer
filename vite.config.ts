9fe448d1e774d98509cd7fc49bdd1edc2950df89
aminnausin <aminnausin@outlook.com>
2025-06-03 11:54:57 -0400

    feat: testing commands and configuration with coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,38 @@
 // import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath } from 'node:url';
 
 import viteCompression from 'vite-plugin-compression';
 import laravel from 'laravel-vite-plugin';
 import Icons from 'unplugin-icons/vite';
 import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'node:url';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
+const env = loadEnv('', process.cwd());
 
-    return {
-        mode: env.APP_ENV === 'local' ? 'development' : 'production',
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.ts'],
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    mode: env.APP_ENV === 'production' ? 'production' : 'development',
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-            Icons({
-                autoInstall: true,
-            }),
-            viteCompression(),
-        ],
-        resolve: {
-            alias: {
-                '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
-                vue: 'vue/dist/vue.esm-bundler.js',
             },
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+        viteCompression(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+            vue: 'vue/dist/vue.esm-bundler.js',
         },
-    };
+    },
 });
---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            exclude: [...configDefaults.exclude, 'e2e/**'],
+            coverage: {
+                provider: 'v8',
+                include: ['resources/js/**/*.{ts,tsx,js,vue}'],
+                exclude: ['node_modules', 'vendor', 'public', 'resources/js/types', 'resources/js/**/__tests__/**'],
+            },
+        },
+    }),
+);
