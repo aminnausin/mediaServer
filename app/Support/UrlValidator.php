9ca02eea4de636cefd0cc733ecbfb38164e17972
aminnausin <aminnausin@outlook.com>
2025-06-03 11:06:00 -0400

    chore: pint

--- app/Support/UrlValidator.php
+++ app/Support/UrlValidator.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Str;
 class UrlValidator {
     public function isExternalUrl(string $url): bool {
         //  ! strpos($metadata->poster_url, str_replace('http://', '', str_replace('https://', '', config('api.app_url')))
-        return $url && !Str::startsWith($url, config('app.scheme') . "://" . config('app.host'));
+        return $url && ! Str::startsWith($url, config('app.scheme') . '://' . config('app.host'));
     }
 }
