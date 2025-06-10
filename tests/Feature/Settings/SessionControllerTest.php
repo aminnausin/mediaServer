543b93e85c84f27b6cbd071a8351c5cd4f750381
aminnausin <aminnausin@outlook.com>
2025-06-10 00:04:43 -0400

    chore: session test namespace

--- tests/Feature/Settings/SessionControllerTest.php
+++ tests/Feature/Settings/SessionControllerTest.php
@@ -30,12 +30,12 @@ class SessionControllerTest extends TestCase {
         $request = Request::create('/fake', 'GET', [], [
             config('session.cookie') => $sessionId,
         ]);
-        $request->setUserResolver(fn () => $user);
+        $request->setUserResolver(fn() => $user);
         $request->setLaravelSession(app('session.store'));
         app('session.store')->setId($sessionId);
         app('session.store')->start();
 
-        $controller = new SessionController;
+        $controller = new \App\Http\Controllers\API\V1\SessionController();
         $response = TestResponse::fromBaseResponse($controller->index($request));
 
         $response->assertOk();
