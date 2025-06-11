f6ccc77e0ef373f981630aac2b8ba498fc2b9088
aminnausin <aminnausin@outlook.com>
2025-06-11 01:52:05 -0400

    feat: backend for deleting account

--- app/Http/Controllers/Api/V1/ProfileController.php
+++ app/Http/Controllers/Api/V1/ProfileController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller {
@@ -37,5 +38,22 @@ class ProfileController extends Controller {
         } catch (\Throwable $th) {
             return $this->error(null, 'Unable to get user. Error: ' . $th->getMessage(), 500);
         }
+    }
+
+    public function destroy(Request $request): Response {
+        $request->validate([
+            'password' => ['required', 'current_password'],
+        ]);
+
+        $user = $request->user();
+
+        Auth::logout();
+
+        $user->delete();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
     }
 }
