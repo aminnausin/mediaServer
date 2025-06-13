f2f76e199fec5f9903a89671551485abe5a19cff
aminnausin <aminnausin@outlook.com>
2025-06-12 23:57:19 -0400

    chore: remove old code

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,7 @@
 <?php
 
 use App\Jobs\ScheduledIndexFiles;
-// use Illuminate\Foundation\Inspiring;
-// use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-// Artisan::command('inspire', function () {
-//     $this->comment(Inspiring::quote());
-// })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::job(new ScheduledIndexFiles)->everySixHours()->environments(['staging', 'production']);
 Schedule::command('auth:clear-resets')->everyFifteenMinutes();
