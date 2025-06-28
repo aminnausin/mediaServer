135d5486d12e591f2e7b6ee14705fd174492ce03
aminnausin <aminnausin@outlook.com>
2025-06-28 15:42:20 -0400

    feat: basic demo seeder

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Metadata;
+use App\Models\Record;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DemoSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        User::firstOrCreate(
+            ['email' => 'demo@mediaserver.me'],
+            [
+                'name' => 'demo',
+                'email_verified_at' => now(),
+                'password' => Hash::make('mediaserver'),
+            ]
+        );
+        User::factory(5)->create();
+        Tag::factory(15)->create();
+        $metadata = Metadata::factory()->count(10)->create();
+
+        foreach ($metadata as $entry) {
+            Record::factory()->count(2)->create([
+                'user_id' => 1,
+                'metadata_id' => $entry->id,
+            ]);
+        }
+    }
+}
