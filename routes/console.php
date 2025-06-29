71ee264ee4d61aa906cb4a4f82c3fa0452f122de
aminnausin <aminnausin@outlook.com>
2025-06-29 10:00:34 -0400

    fix: npm php commands missing scheduler

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@
 use App\Jobs\ScheduledIndexFiles;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new ScheduledIndexFiles)->everySixHours()->environments(['staging', 'production']);
+Schedule::job(new ScheduledIndexFiles)->everySixHours()->withoutOverlapping()->environments(['staging', 'production']);
 Schedule::command('auth:clear-resets')->everyFifteenMinutes();
 Schedule::command('sanctum:prune-expired --hours=2')->daily();
 
