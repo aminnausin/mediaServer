706beb0380669e7be8068bcf6c043e86a42d868e
aminnausin <aminnausin@outlook.com>
2025-07-05 18:03:26 -0400

    fix: passing class directly does not work on components with multiple root elements

--- resources/js/components/labels/FormInputLabel.vue
+++ resources/js/components/labels/FormInputLabel.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import type { FormField } from '@/types/types';
 
-defineProps<{
+const props = defineProps<{
     field: FormField;
     textSize?: string;
+    class?: string;
 }>();
 </script>
 
 <template>
-    <label :for="field?.name" :class="[`leading-none block font-medium text-gray-700 dark:text-neutral-200`, textSize ? textSize : 'text-sm']">
+    <label :for="field?.name" :class="[`leading-none block font-medium text-gray-700 dark:text-neutral-200`, textSize ? textSize : 'text-sm', `${props.class ?? ''}`]">
         {{ field?.text }}
     </label>
     <p v-if="field?.subtext" class="text-sm/6 text-gray-500 dark:text-gray-400">{{ field.subtext }}</p>
--- resources/js/components/labels/InputLabel.vue
+++ resources/js/components/labels/InputLabel.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
     name?: string;
     text?: string;
     subtext?: string;
+    class?: string;
     subtextClass?: string;
 }>();
 </script>
 
 <template>
-    <label :for="name" :class="[`font-medium text-gray-700 dark:text-neutral-200`]">
+    <label :for="name" :class="[`font-medium text-gray-700 dark:text-neutral-200`, props.class]">
         {{ text }}
     </label>
     <p v-if="subtext" :class="['text-sm/6 text-gray-500 dark:text-gray-400', subtextClass]">{{ subtext }}</p>
