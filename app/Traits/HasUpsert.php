0c4c6541726a2fe757d3db64e0f2df719eb0c252
aminnausin <83550431+aminnausin@users.noreply.github.com>
2025-07-14 02:08:13 +0000

    chore: lint coding style (pint)

--- app/Traits/HasUpsert.php
+++ app/Traits/HasUpsert.php
@@ -8,14 +8,11 @@ trait HasUpsert {
     /**
      * Generates tag relationships for any model
      *
-     * @param  string       $context        Custom Error Message
-     * @param  Throwable    $th             Original Error
-     * @param  array        $transactions   Upsert Transactions
-     * @param  int          $updateCount    Number of updates attempted
-     * @param  int|null     $checkCount     Number of checks attempted (optional)
-     *
-     * @return never
-     *
+     * @param  string  $context  Custom Error Message
+     * @param  Throwable  $th  Original Error
+     * @param  array  $transactions  Upsert Transactions
+     * @param  int  $updateCount  Number of updates attempted
+     * @param  int|null  $checkCount  Number of checks attempted (optional)
      */
     public function handleError(string $context, \Throwable $th, array $ids, int $updateCount, ?int $checkCount = null): never {
         $message = sprintf(
