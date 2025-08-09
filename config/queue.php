b5dbeacdff1536d752fd18508bbc556cf9c17717
aminnausin <aminnausin@outlook.com>
2025-08-09 12:48:01 -0400

    fix: redis jobs retry automatically early even if long jobs such as embed are still running and have not timed out via horizon

--- config/queue.php
+++ config/queue.php
@@ -77,7 +77,7 @@ return [
             'driver' => 'redis',
             'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
             'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
+            'retry_after' => 172800, //Rely on Horizon to decide retries instead of Laravel -> Original: (int) env('REDIS_QUEUE_RETRY_AFTER', 90),
             'block_for' => null,
             'after_commit' => false,
         ],
