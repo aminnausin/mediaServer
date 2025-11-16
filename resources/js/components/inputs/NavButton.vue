e1f1b9533165f6b79e30eb4c2cf317aedf9bfead
aminnausin <aminnausin@outlook.com>
2025-11-16 13:27:32 -0500

    fix: nav buttons and labels have no cursor

--- resources/js/components/inputs/NavButton.vue
+++ resources/js/components/inputs/NavButton.vue
@@ -7,7 +7,7 @@ const activeColour = ref(`ring-3 ring-inset ring-[0.1rem] dark:ring-0 ring-viole
 <template>
     <button
         :id="'btn-nav-' + props.label"
-        :class="`${props.active ? activeColour : `bg-white dark:bg-primary-dark-800`} hover:bg-neutral-100 dark:hover:bg-violet-700 dark:text-neutral-100 flex justify-center items-center shrink-0 h-8 w-8 rounded-lg shadow-lg`"
+        :class="`${props.active ? activeColour : `dark:bg-primary-dark-800 bg-white`} flex h-8 w-8 shrink-0 cursor-pointer items-center justify-center rounded-lg shadow-lg hover:bg-neutral-100 dark:text-neutral-100 dark:hover:bg-violet-700`"
         :aria-label="props.label"
     >
         <slot name="icon"></slot>
--- resources/js/components/labels/InputLabel.vue
+++ resources/js/components/labels/InputLabel.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 </script>
 
 <template>
-    <label :for="name" :class="[`font-medium text-gray-700 dark:text-neutral-200`, props.class]">
+    <label :for="name" :class="[`cursor-pointer font-medium text-gray-700 dark:text-neutral-200`, props.class]">
         {{ text }}
     </label>
     <p v-if="subtext" :class="['text-sm/6 text-gray-500 dark:text-gray-400', subtextClass]">{{ subtext }}</p>
