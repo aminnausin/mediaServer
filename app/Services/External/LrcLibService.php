05c28426e13ba444bc9c9a0249e997bfcbc7052a
aminnausin <aminnausin@outlook.com>
2025-12-06 12:48:44 -0500

    feat: lrc lib logging

--- app/Services/External/LrcLibService.php
+++ app/Services/External/LrcLibService.php
@@ -5,29 +5,47 @@ namespace App\Services\External;
 use App\Models\Metadata;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class LrcLibService {
     public function importLyrics(Metadata $metadata): array {
+        $url = 'https://lrclib.net/api/get';
+
         $query = array_filter([
-            'track_name' => $metadata->title,
+            'track_name' => $metadata->title ?? $metadata->name,
             'artist_name' => $metadata->artist ?? explode(' - ', $metadata->description)[0],
             'album_name' => $metadata->album,
             'duration' => $metadata->duration,
-        ], fn ($value) => $value !== null && $value !== '');
+        ], fn($value) => $value !== null && $value !== '');
 
-        $response = Http::get('https://lrclib.net/api/get', $query);
+        $response = Http::get($url, $query);
+
+        Log::info("Lrclib Get", [
+            'url' => $url . '?' . http_build_query($query),
+            'payload' => $query,
+            'response' => $response->json()
+        ]);
 
         return ['lrclib' => $response->json(), 'payload' => $query];
     }
 
     public function searchLyrics(Metadata $metadata, Request $request): array {
-        $query = array_filter([
-            'track_name' => $request->query('track') ?? $metadata->title,
-            'artist_name' => $request->query('artist') ?? $metadata->artist ?? explode(' - ', $metadata->description)[0],
-            'album_name' => $request->query('album') ?? $metadata->album,
-        ], fn ($value) => $value !== null && $value !== '');
+        $url = 'https://lrclib.net/api/search';
 
-        $response = Http::get('https://lrclib.net/api/search', $query);
+        // This should not use the defaults so that the user can edit the search to blanks
+        $query = array_filter([
+            'track_name' => $request->query('track'), // ?? $metadata->title,
+            'artist_name' => $request->query('artist'), // ?? $metadata->artist ?? explode(' - ', $metadata->description)[0],
+            'album_name' => $request->query('album'), // ?? $metadata->album,
+        ], fn($value) => $value !== null && $value !== '');
+
+        $response = Http::get($url, $query);
+
+        Log::info("Lrclib Search", [
+            'url' => $url . '?' . http_build_query($query),
+            'payload' => $query,
+            'response' => $response->json()
+        ]);
 
         return ['lrclib' => $response->json(), 'payload' => $query];
     }
