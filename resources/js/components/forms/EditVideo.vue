dc8cd773ed8ae0e2c4980ab5d3469fac18577cab
aminnausin <aminnausin@outlook.com>
2025-12-15 23:48:18 -0500

    fix: lyrics are not editable on music videos

--- resources/js/components/forms/EditVideo.vue
+++ resources/js/components/forms/EditVideo.vue
@@ -5,8 +5,10 @@ import type { MetadataUpdateRequest } from '@/types/requests';
 
 import { computed, reactive, ref, watch } from 'vue';
 import { toCalendarFormattedDate } from '@/service/util';
+import { useContentStore } from '@/stores/ContentStore';
 import { useGetAllTags } from '@/service/queries';
 import { UseCreateTag } from '@/service/mutations';
+import { storeToRefs } from 'pinia';
 import { MediaType } from '@/types/types';
 import { toast } from '@/service/toaster/toastService';
 
@@ -19,6 +21,8 @@ import DatePicker from '@/components/pinesUI/DatePicker.vue';
 import FormInput from '@/components/inputs/FormInput.vue';
 import mediaAPI from '@/service/mediaAPI.ts';
 import useForm from '@/composables/useForm';
+
+const { stateFolder } = storeToRefs(useContentStore());
 
 const emit = defineEmits(['handleFinish']);
 const props = defineProps<{ video: VideoResource }>();
@@ -56,7 +60,7 @@ const fields = reactive<FormField[]>([
         value: props.video?.metadata?.lyrics,
         subtext: 'Format: [mm:ss] line',
         placeholder: `No lyrics yet.`,
-        disabled: !isAudio.value,
+        disabled: !isAudio.value && !stateFolder.value.is_majority_audio,
         default: '',
     },
     {
@@ -213,7 +217,7 @@ watch(tagsQuery, () => {
 </script>
 
 <template>
-    <form class="flex flex-col sm:flex-row sm:justify-between flex-wrap gap-4" @submit.prevent="handleSubmit">
+    <form class="flex flex-col flex-wrap gap-4 sm:flex-row sm:justify-between" @submit.prevent="handleSubmit">
         <div v-for="(field, index) in fields.filter((field) => !field.disabled)" :key="index" class="w-full" :class="field.class">
             <FormInputLabel :field="field" class="capitalize" />
 
@@ -234,12 +238,12 @@ watch(tagsQuery, () => {
             <FormErrorList :errors="form.errors" :field-name="field.name" />
         </div>
 
-        <div class="relative flex flex-col-reverse sm:flex-row sm:justify-end gap-2 pt-1 w-full">
+        <div class="relative flex w-full flex-col-reverse gap-2 pt-1 sm:flex-row sm:justify-end">
             <button
                 @click="$emit('handleFinish')"
                 type="button"
-                class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors border dark:border-neutral-600 rounded-md focus:outline-hidden"
-                :class="'focus:ring-1 focus:ring-neutral-100 dark:focus:ring-neutral-400 focus:ring-offset-1 hover:bg-neutral-100 dark:hover:bg-neutral-900'"
+                class="inline-flex h-10 items-center justify-center rounded-md border px-4 py-2 text-sm font-medium transition-colors focus:outline-hidden dark:border-neutral-600"
+                :class="'hover:bg-neutral-100 focus:ring-1 focus:ring-neutral-100 focus:ring-offset-1 dark:hover:bg-neutral-900 dark:focus:ring-neutral-400'"
                 :disabled="form.processing"
             >
                 Cancel
@@ -247,8 +251,8 @@ watch(tagsQuery, () => {
             <button
                 @click="handleSubmit"
                 type="button"
-                class="inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium text-white transition-colors border border-transparent rounded-md focus:outline-hidden"
-                :class="'focus:ring-1 focus:ring-violet-900 focus:ring-offset-1 bg-neutral-950 hover:bg-neutral-800 dark:hover:bg-neutral-900 '"
+                class="inline-flex h-10 items-center justify-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white transition-colors focus:outline-hidden"
+                :class="'bg-neutral-950 hover:bg-neutral-800 focus:ring-1 focus:ring-violet-900 focus:ring-offset-1 dark:hover:bg-neutral-900'"
                 :disabled="form.processing"
             >
                 Submit Details
