87d0768eb47c662aa1634f2feb16830ebdf72a6d
aminnausin <aminnausin@outlook.com>
2026-01-14 02:36:21 -0500

    fix: modal tooltip should be relative to prevent on-click-outside

--- resources/js/components/cedar-ui/modal/BaseModal.vue
+++ resources/js/components/cedar-ui/modal/BaseModal.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
+import { RelativeHoverCard } from '@/components/cedar-ui/hover-card';
 import { useModalStore } from '@/stores/ModalStore';
 import { ButtonCorner } from '@/components/cedar-ui/button';
-import { HoverCard } from '@/components/cedar-ui/hover-card';
 
 const modalStore = useModalStore();
 </script>
 
 <template>
     <section v-if="!modalStore.props?.hideHeader" class="flex flex-wrap items-center gap-2">
-        <HoverCard class="flex-1 scroll-mt-16 sm:scroll-mt-12" :content="modalStore.props.titleTooltip">
+        <RelativeHoverCard class="flex-1 scroll-mt-16 sm:scroll-mt-12" :content="modalStore.props.titleTooltip">
             <template #trigger>
                 <h3 ref="modalTitle" id="modalTitle" class="text-xl font-semibold">
                     <slot name="title">
@@ -16,7 +16,7 @@ const modalStore = useModalStore();
                     </slot>
                 </h3>
             </template>
-        </HoverCard>
+        </RelativeHoverCard>
         <ButtonCorner @click="modalStore.close" class="static! m-0!" />
         <p class="text-foreground-2 w-full text-sm" v-if="$slots.description" id="modalDescription">
             <slot name="description"> </slot>
