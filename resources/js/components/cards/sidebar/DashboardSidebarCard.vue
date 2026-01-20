a31fea4707dc1629096181b8407cb3dc8696862e
aminnausin <aminnausin@outlook.com>
2026-01-20 13:09:22 -0500

    fix: Dashboard and settings sidebars stay "open" on navigation on small screens Fixes #228

--- resources/js/components/cards/sidebar/DashboardSidebarCard.vue
+++ resources/js/components/cards/sidebar/DashboardSidebarCard.vue
@@ -7,7 +7,7 @@ defineProps<{ isActive?: boolean }>();
 </script>
 
 <template>
-    <SidebarCard :class="cn('gap-2', 'items-center justify-between', 'overflow-hidden capitalize', 'aria-disabled:button-disabled', { 'ring-primary ring-2': isActive })">
+    <SidebarCard :class="cn('gap-2', 'items-center justify-between', 'overflow-hidden capitalize', 'aria-disabled:button-disabled', { 'ring-primary/70 ring-2': isActive })">
         <template #header><slot name="header" /></template>
         <template #body><slot name="body" /></template>
     </SidebarCard>
