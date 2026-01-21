70d971c75df8c2c97d759cdf1400e54ac2ccec11
aminnausin <aminnausin@outlook.com>
2026-01-21 17:13:01 -0500

    feat: libass subtitle renderer integrated with player

---
+++ resources/js/components/video/subtitles/OctopusRenderer.ts
@@ -0,0 +1,38 @@
+import { ref } from 'vue';
+
+import SubtitlesOctopus from '@/lib/libass-wasm/subtitles-octopus';
+
+export default function useOctopusRenderer() {
+    const assInstance = ref<any>(null);
+
+    const instantiateOctopus = async (subUrl: string = '/test/2.ass') => {
+        clearOctopus();
+        const video = document.getElementById('video-source');
+        if (!video) return;
+        const options = {
+            video, // HTML5 video element
+            subUrl, // Link to subtitles
+            fonts: ['/fonts/Roboto-Medium.ttf', '/fonts/Arial.ttf'],
+            workerUrl: '/lib/subtitles-octopus/subtitles-octopus-worker.js',
+            legacyWorkerUrl: '/lib/subtitles-octopus/subtitles-octopus-worker-legacy.js',
+        };
+        assInstance.value = new SubtitlesOctopus(options);
+    };
+
+    const clearOctopus = () => {
+        if (assInstance.value && assInstance.value.worker) {
+            assInstance.value.dispose();
+            assInstance.value = null;
+        }
+    };
+
+    const resizeOctopus = () => {
+        if (assInstance.value && assInstance.value.worker) assInstance.value.resize();
+    };
+
+    return {
+        instantiateOctopus,
+        clearOctopus,
+        resizeOctopus,
+    };
+}
--- resources/js/components/video/subtitles/PlayerSubtitles.vue
+++ resources/js/components/video/subtitles/PlayerSubtitles.vue
@@ -11,17 +11,21 @@ import { cn } from '@aminnausin/cedar-ui';
 
 import VideoPopoverSlider from '@/components/video/VideoPopoverSlider.vue';
 import VideoPopoverItem from '@/components/video/VideoPopoverItem.vue';
+import SubtitlesOctopus from '@/lib/libass-wasm/subtitles-octopus';
 import VideoPopover from '@/components/video/VideoPopover.vue';
 
 import ProiconsTextFontSize from '~icons/proicons/text-font-size';
 import ProiconsCheckmark from '~icons/proicons/checkmark';
 import LucideCaptionsOff from '~icons/lucide/captions-off';
 import LucideCaptions from '~icons/lucide/captions';
+import useOctopusRenderer from './OctopusRenderer';
 
 interface PlayerSubtitlesProps {
     videoButtonOffset: number;
     usingPlayerModernUI?: boolean;
 }
+
+const { instantiateOctopus, clearOctopus, resizeOctopus } = useOctopusRenderer();
 
 //#region Shared State
 const { stateVideo } = storeToRefs(useContentStore());
@@ -38,6 +42,7 @@ const subtitleSizeMax = 2;
 const subtitleSizeDelta = 0.1;
 
 const subtitlesPopover = useTemplateRef('subtitles-popover');
+
 const playerSubtitleItems = computed(() => {
     const items: PopoverItem[] = stateVideo.value.subtitles.map((track) => {
         const isCurrentTrack = isShowingSubtitles.value && currentSubtitleTrack.value?.track_id === track.track_id;
@@ -113,8 +118,13 @@ const handleSubtitles = (track?: SubtitleResource) => {
 
     if (!player?.value) return;
 
-    for (const textTrack of player.value.textTracks) {
-        textTrack.mode = isShowingSubtitles.value && textTrack.language === currentSubtitleTrack.value?.language ? 'showing' : 'hidden';
+    if (nextTrack?.codec === 'ass') {
+        instantiateOctopus(`/data/subtitles/${nextTrack.metadata_uuid}/${nextTrack.track_id}${nextTrack.track_id === 0 ? `.${nextTrack.language}` : ''}.ass`);
+        hideAllTracks();
+    } else {
+        for (const textTrack of player.value.textTracks) {
+            textTrack.mode = isShowingSubtitles.value && textTrack.language === currentSubtitleTrack.value?.language ? 'showing' : 'hidden';
+        }
     }
 };
 
@@ -122,12 +132,15 @@ const handleSubtitles = (track?: SubtitleResource) => {
  * Set subtitles to blank.
  */
 const clearSubtitles = () => {
+    clearOctopus();
     currentSubtitleTrack.value = undefined;
     isShowingSubtitles.value = false;
     subtitlesPopover.value?.handleClose();
+    hideAllTracks();
+};
 
+const hideAllTracks = () => {
     if (!player?.value) return;
-
     for (const textTrack of player.value.textTracks) {
         textTrack.mode = 'hidden';
     }
@@ -137,6 +150,7 @@ const clearSubtitles = () => {
 defineExpose({
     handleSubtitles,
     clearSubtitles,
+    resizeOctopus,
     isShowingSubtitles,
     currentSubtitleTrack,
     defaultSubtitleTrack,
@@ -164,7 +178,7 @@ defineExpose({
         <template #content>
             <section :class="['scrollbar-minimal flex max-h-32 flex-col overflow-y-auto transition-transform md:h-fit', { 'h-25 pe-0.5': playerSubtitleItems.length > 1 }]">
                 <VideoPopoverSlider
-                    v-if="playerSubtitleItems.length > 1"
+                    v-if="playerSubtitleItems.length > 1 && currentSubtitleTrack && currentSubtitleTrack?.codec !== 'ass'"
                     v-model="subtitleSizeMultiplier"
                     :text="`Font Size`"
                     :shortcut="`${Math.round(subtitleSizeMultiplier * 100)}%`"
@@ -181,3 +195,8 @@ defineExpose({
         </template>
     </VideoPopover>
 </template>
+<style lang="css">
+.libassjs-canvas-parent {
+    z-index: 3;
+}
+</style>
