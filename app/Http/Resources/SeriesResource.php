1cd3a015ab5e271fa080e74d0e44f4c41ed5265c
aminnausin <aminnausin@outlook.com>
2026-01-26 14:16:13 -0500

    fix: was using date_action instead of action_at on series resource

--- app/Http/Resources/SeriesResource.php
+++ app/Http/Resources/SeriesResource.php
@@ -9,6 +9,31 @@ class SeriesResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
+     * id                   -> int8 (pk) (index)
+     * folder_id            -> int8 (fk) (nullable) (index)
+     * editor_id            -> int8 (fk) (nullable)
+     * composite_id         -> varchar(255) (index)
+     *
+     * primary_media_type   -> int2 (enum)
+     * total_size           -> int8
+     *
+     * title                -> varchar(255) (nullable)
+     * description          -> text (nullable)
+     * studio               -> varchar(255) (nullable)
+     * rating               -> int2 (nullable)
+     * seasons              -> int4 (nullable)
+     * episodes             -> int4 (nullable)
+     * films                -> int4 (nullable)
+     * thumbnail_url        -> varchar(255) (nullable)
+     * raw_thumbnail_url    -> varchar(255) (nullable)
+     *
+     * date_start           -> date (nullable)
+     * date_end             -> date (nullable)
+     *
+     * created_at           -> timestamp (nullable)
+     * updated_at           -> timestamp (nullable)
+     * edited_at            -> timestamp (nullable)
+     *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array {
@@ -27,8 +52,11 @@ class SeriesResource extends JsonResource {
             'date_start' => $this->date_start,
             'date_end' => $this->date_end,
             'thumbnail_url' => $this->thumbnail_url,
-            'date_updated' => $this->updated_at,
-            'date_created' => $this->created_at,
+            // 'date_updated' => $this->updated_at,
+            // 'date_created' => $this->created_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'edited_at' => $this->edited_at,
         ];
     }
 }
