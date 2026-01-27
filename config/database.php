545452503301f9aba1eaef0523be0f339a0d3cdd
aminnausin <aminnausin@outlook.com>
2026-01-26 20:50:53 -0500

    fix: database connection timezone is not guaranteed

--- config/database.php
+++ config/database.php
@@ -95,6 +95,7 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => 'prefer',
+            'timezone' => '+00:00',
         ],
 
         'sqlsrv' => [
