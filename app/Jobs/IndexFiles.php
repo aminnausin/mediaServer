9b63add065ca591bc242f41cedfcae8c5b6c4430
aminnausin <aminnausin@outlook.com>
2026-02-02 01:13:49 -0500

    fix: typo caused mp4 and m4a files to get untracked

--- app/Jobs/IndexFiles.php
+++ app/Jobs/IndexFiles.php
@@ -441,7 +441,7 @@ class IndexFiles extends ManagedSubTask {
                 // TODO: This line defines what file types are supported. Move this somewhere else that is easy to configure
                 $ext = pathinfo($file, PATHINFO_EXTENSION);
                 $normalisedExt = strtolower($ext);
-                if (! in_array($normalisedExt, ['mp4' . 'm4a', 'mkv', 'mp3', 'ogg', 'flac', 'webm'])) { // the conversion breaks ogg idk about flac
+                if (! in_array($normalisedExt, ['mp4', 'm4a', 'mkv', 'mp3', 'ogg', 'flac', 'webm'])) { // the conversion breaks ogg idk about flac
                     continue;
                 }
 
