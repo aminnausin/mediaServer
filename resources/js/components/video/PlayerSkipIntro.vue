f8f27c76e84c757deb5dcef2411e95e01df69215
aminnausin <aminnausin@outlook.com>
2026-02-23 23:10:09 -0500

    chore: skip intro debug

--- resources/js/components/video/PlayerSkipIntro.vue
+++ resources/js/components/video/PlayerSkipIntro.vue
@@ -39,6 +39,12 @@ const intro = computed(() => {
 });
 </script>
 <template>
+    <div class="text-foreground-0 pointer-events-auto absolute top-2 left-2 flex flex-col gap-2" style="z-index: 10">
+        <!-- <FormNumberField :field="{ name: 'introStart', type: 'number', min: 0, default: 0 }" v-model="introStart" /> -->
+        <!-- <FormNumberField :field="{ name: 'introDuration', type: 'number', min: 0, default: 90 }" v-model="introDuration" /> -->
+        <p class="bg-surface-0 text-foreground-0 p-1">{{ introStart === undefined ? 'und' : (introStart ?? 'null') }}</p>
+        <p class="bg-surface-0 text-foreground-0 p-1">{{ introDuration }}</p>
+    </div>
     <Transition enter-from-class="opacity-0" enter-to-class="opacity-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
         <div
             :class="['absolute bottom-22 transition-opacity duration-200 ease-in-out', isNormalView ? 'left-2' : 'left-4']"
