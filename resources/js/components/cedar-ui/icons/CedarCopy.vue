79da3004e2b01fcce26fe136a9f5e1ef5a869f7c
aminnausin <aminnausin@outlook.com>
2026-02-26 01:50:57 -0500

    feat: CedarCopy icon

---
+++ resources/js/components/cedar-ui/icons/CedarCopy.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg class="size-4 stroke-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" stroke="none">
+            <path
+                d="M7.75 7.757V6.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-.992"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></path>
+            <path
+                d="M3.75 10.75a3 3 0 0 1 3-3h6.5a3 3 0 0 1 3 3v6.5a3 3 0 0 1-3 3h-6.5a3 3 0 0 1-3-3v-6.5z"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            ></path>
+        </g>
+    </svg>
+</template>
