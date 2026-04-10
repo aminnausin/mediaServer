57f619e3ed26dd4d613dd48bb09ab4e4acfd1459
aminnausin <aminnausin@outlook.com>
2026-04-10 00:30:43 -0400

    fix: download icon stroke

--- resources/js/components/icons/TablerDownload.vue
+++ resources/js/components/icons/TablerDownload.vue
@@ -5,7 +5,7 @@
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round"
-            stroke-width="1"
+            stroke-width="1.5"
             d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2M7 11l5 5l5-5m-5-7v12"
         />
     </svg>
--- resources/js/components/icons/TablerDownloadOff.vue
+++ resources/js/components/icons/TablerDownloadOff.vue
@@ -5,7 +5,7 @@
             stroke="currentColor"
             stroke-linecap="round"
             stroke-linejoin="round"
-            stroke-width="1"
+            stroke-width="1.5"
             d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 1.83-1.19M7 11l5 5l2-2m2-2l1-1m-5-7v4m0 4v4M3 3l18 18"
         />
     </svg>
