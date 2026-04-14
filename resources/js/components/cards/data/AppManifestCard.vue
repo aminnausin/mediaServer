a262c20415aa3fccc6596da593879e730bcab765
aminnausin <aminnausin@outlook.com>
2026-04-13 21:59:11 -0400

    fix: app manifest card pointer always shows

--- resources/js/components/cards/data/AppManifestCard.vue
+++ resources/js/components/cards/data/AppManifestCard.vue
@@ -26,19 +26,19 @@ const copyToClipboard = async () => {
 </script>
 
 <template>
-    <DashboardSidebarCard class="pointer-default">
+    <DashboardSidebarCard class="cursor-default">
         <template #header>
-            <a title="GitHub" class="hover:text-primary dark:hover:text-primary-muted contents" href="https://github.com/aminnausin/mediaServer/" target="_blank">
+            <a title="GitHub" class="hover-text-primary contents" href="https://github.com/aminnausin/mediaServer/" target="_blank">
                 <h3 :title="'Source Code'" class="truncate">MediaServer</h3>
                 <ProiconsGithub class="ml-auto size-5 shrink-0" />
             </a>
         </template>
         <template #body>
-            <h4 title="App Version - Click to copy" class="hover:text-primary dark:hover:text-primary-muted w-full flex-1 text-nowrap sm:truncate" @click.prevent="copyToClipboard">
-                V{{ appManifest.version ?? '0.1.15b' }}
-            </h4>
+            <button @click="copyToClipboard" title="Copy App Version" class="flex w-full flex-1 sm:truncate">
+                <h4 class="hover-text-primary cursor-pointer text-nowrap sm:truncate">V{{ appManifest.version ?? '0.1.15b' }}</h4>
+            </button>
             <a v-if="appManifest.commit" target="_blank" :href="`https://github.com/aminnausin/mediaServer/commit/${appManifest.commit}`">
-                <h4 title="Latest Commit" class="hover:text-primary dark:hover:text-primary-muted w-fit truncate text-nowrap">#{{ appManifest.commit }}</h4>
+                <h4 title="Latest Commit" class="hover-text-primary w-fit truncate text-nowrap">#{{ appManifest.commit }}</h4>
             </a>
         </template>
     </DashboardSidebarCard>
