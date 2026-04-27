52c39cbd81a056a621d74f4dfb88a387c14bc53a
aminnausin <aminnausin@outlook.com>
2026-04-27 12:01:56 -0400

    feat: spectrograph icon

---
+++ resources/js/components/icons/IconSpectrograph.vue
@@ -0,0 +1,12 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.5"
+            d="M12 20.75V3.25m8 11.71V9.04M4 14.96V9.04m12 8.872V6.088M8 17.912V6.088"
+        />
+    </svg>
+</template>
