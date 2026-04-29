56571f03bfe63c772fbfc0850137612f63282072
aminnausin <aminnausin@outlook.com>
2026-04-28 20:08:54 -0400

    fix: table pagination layers

--- resources/js/components/cedar-ui/table/TablePaginationButton.vue
+++ resources/js/components/cedar-ui/table/TablePaginationButton.vue
@@ -13,16 +13,18 @@ const props = withDefaults(defineProps<TablePaginationButtonProps>(), {
 <template>
     <li :class="{ hidden: currentPage !== pageNumber && !text && !sticky }" class="h-full @xs:block">
         <button-base
-            class="group hover:text-foreground-0 disabled:text-foreground-2 pointer-events-auto! relative inline-flex h-full rounded-none px-3"
-            :class="{
-                'text-foreground-0 bg-overlay-accent': currentPage === pageNumber,
-                'hover:bg-overlay-accent': !disabled,
-            }"
+            :class="
+                cn('group hover:text-foreground-0 disabled:text-foreground-2/60 pointer-events-auto! flex h-full flex-col gap-0 rounded-none py-0 opacity-100!', {
+                    'text-foreground-0 bg-overlay-accent': currentPage === pageNumber,
+                    'hover:bg-overlay-accent': !disabled,
+                    'px-3': !underline && $slots.content,
+                })
+            "
             :disabled="disabled ?? false"
             :use-size="false"
         >
             <slot name="content">
-                <span>
+                <span class="-mb-px flex h-full flex-1 items-center px-3 text-center">
                     {{ text ?? pageNumber }}
                 </span>
             </slot>
@@ -31,7 +33,9 @@ const props = withDefaults(defineProps<TablePaginationButtonProps>(), {
                 v-if="!text || underline"
                 :class="
                     cn(
-                        'bg-primary group-hover:border-primary absolute bottom-0 -mx-px box-content h-px w-0 translate-y-px border-transparent duration-(--duration-input) ease-out group-hover:left-0 group-hover:w-full group-hover:border-r group-hover:border-l',
+                        'bg-primary border-transparent duration-(--duration-input) ease-out',
+                        '-mx-px mt-auto box-content h-px w-0 translate-y-px',
+                        'group-hover:border-primary group-hover:left-0 group-hover:w-full group-hover:border-x',
                         {
                             'left-0 w-full border-r border-l': currentPage === pageNumber,
                             'left-1/2': currentPage !== pageNumber,
