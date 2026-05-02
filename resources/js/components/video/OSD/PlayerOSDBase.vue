7eded2b1325a0e6df93e44588d46130fae70abbc
aminnausin <aminnausin@outlook.com>
2026-05-02 14:10:32 -0400

    fix: missing border and blur on osd

--- resources/js/components/video/OSD/PlayerOSDBase.vue
+++ resources/js/components/video/OSD/PlayerOSDBase.vue
@@ -2,7 +2,7 @@
 import { cn } from '@aminnausin/cedar-ui';
 </script>
 <template>
-    <div :class="cn('flex items-center justify-center rounded-full bg-neutral-900/60 p-3 text-white', $attrs.class)">
+    <div :class="cn('flex items-center justify-center rounded-full border border-neutral-700/10 bg-neutral-900/60 p-3 text-white backdrop-blur-xs', $attrs.class)">
         <slot> </slot>
     </div>
 </template>
