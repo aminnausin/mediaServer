a11fdf9c39d6d565adc23c87e6975e74d4c1b0a4
aminnausin <aminnausin@outlook.com>
2026-05-05 23:31:41 -0400

    fix: server settings are accessible by url to non admin users

--- resources/js/components/settings/SettingsServer.vue
+++ resources/js/components/settings/SettingsServer.vue
@@ -2,13 +2,22 @@
 import type { BreadCrumbItem } from '@/types/types';
 
 import { BreadCrumbs } from '@/components/cedar-ui/breadcrumbs';
+import { useRouter } from 'vue-router';
 import { computed } from 'vue';
+import { useAuth } from '@/composables/auth/useAuth';
 
 import SettingsServerPerformance from '@/components/settings/server/SettingsServerPerformance.vue';
 import SettingsServerIndexing from '@/components/settings/server/SettingsServerIndexing.vue';
 import SettingsServerStorage from '@/components/settings/server/SettingsServerStorage.vue';
 
 import ProiconsSettings from '~icons/proicons/settings';
+
+const { isAdmin } = useAuth();
+const router = useRouter();
+
+if (!isAdmin.value) {
+    router.replace('/settings');
+}
 
 const breadCrumbs = computed(() => {
     const items: BreadCrumbItem[] = [
