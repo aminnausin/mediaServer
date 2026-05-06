e345b0d099cba2e689d13da9762beec6c05f7ce0
aminnausin <aminnausin@outlook.com>
2026-05-05 23:54:57 -0400

    fix: no error log for failed horizon restarts

--- app/Services/Server/QueueControlService.php
+++ app/Services/Server/QueueControlService.php
@@ -3,12 +3,17 @@
 namespace App\Services\Server;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 use Laravel\Horizon\Contracts\MasterSupervisorRepository;
 
 class QueueControlService {
     public function restart(): void {
-        if (config('queue.default') === 'redis' && $this->horizonIsRunning()) {
-            Artisan::call('horizon:terminate');
+        try {
+            if (config('queue.default') === 'redis' && $this->horizonIsRunning()) {
+                Artisan::call('horizon:terminate');
+            }
+        } catch (\Throwable $th) {
+            Log::error("Unable to restart Horizon", ["error" => $th->getMessage()]);
         }
     }
 
