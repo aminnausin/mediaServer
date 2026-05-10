79be2634846e216a6f9d6efcb6f6eb6d433613eb
aminnausin <aminnausin@outlook.com>
2026-05-10 19:04:09 -0400

    fix: settings card does not gracefully accept styling

--- resources/js/components/cards/layout/SettingsCard.vue
+++ resources/js/components/cards/layout/SettingsCard.vue
@@ -1,5 +1,8 @@
+<script setup lang="ts">
+import { cn } from '@aminnausin/cedar-ui';
+</script>
 <template>
-    <section class="dark:bg-primary-dark-800/70 flex flex-wrap gap-3 rounded-xl bg-white p-3 shadow-md ring-1 ring-gray-900/5 sm:flex-nowrap sm:p-6">
+    <section :class="cn('dark:bg-primary-dark-800/70 flex flex-wrap gap-3 rounded-xl bg-white p-3 shadow-md ring-1 ring-gray-900/5 sm:flex-nowrap sm:p-6', $attrs.class)">
         <slot name="heading"></slot>
 
         <slot name="content"></slot>
