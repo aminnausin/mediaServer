fa6f9779d419ba2c9bda92022dafc17e7dfd40b3
aminnausin <aminnausin@outlook.com>
2026-05-14 17:27:02 -0400

    feat: static woff2 font files for offline compatibility

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,7 @@
     </script>
 
     <link rel="manifest" href="/manifest.json" crossorigin="use-credentials">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Figtree:ital,wght@0,300..900;1,300..900&family=Quicksand:wght@300..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap">
-    <link rel="stylesheet" media="print" onload="this.media='all'" href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Figtree:ital,wght@0,300..900;1,300..900&family=Quicksand:wght@300..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap">
+    <link rel="stylesheet" href="/fonts/fonts.css">
 
     <!-- local -->
     @vite('resources/css/app.css')
