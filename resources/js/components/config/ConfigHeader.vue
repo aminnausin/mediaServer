fac0afbf04dcf2a55d0350ac7a2ea712710d9ff0
aminnausin <aminnausin@outlook.com>
2026-05-13 23:42:33 -0400

    feat: animate dirty flag on titles

--- resources/js/components/config/ConfigHeader.vue
+++ resources/js/components/config/ConfigHeader.vue
@@ -6,7 +6,10 @@ defineProps<{ heading: string; subtext?: string; dirty?: boolean }>();
 
 <template>
     <SettingsHeader class="block max-w-none">
-        <h3 class="3xl:text-xl text-base font-medium">{{ heading }} <span v-if="dirty" class="text-danger"> *</span></h3>
+        <h3 class="3xl:text-xl text-base font-medium">
+            {{ heading }}
+            <span :class="['transition-input text-danger transition-reveal overflow-hidden duration-75', dirty ? 'w-0 ps-1 opacity-100' : 'w-0 ps-0 opacity-0']">*</span>
+        </h3>
         <p class="text-foreground-2">
             <slot>{{ subtext }}</slot>
         </p>
