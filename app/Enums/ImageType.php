bdd4a0a8f379c220265fac0f15b3e27b66628d0e
aminnausin <aminnausin@outlook.com>
2026-05-23 15:06:19 -0400

    chore: remove redundant functions from enums

--- app/Enums/ImageType.php
+++ app/Enums/ImageType.php
@@ -2,11 +2,10 @@
 
 namespace App\Enums;
 
-enum HardwareType: int {
-    case CPU = 0;
-    case CUDA = 1;
-    case QSV = 2;
-    case VAAPI = 3;
+enum ImageType: int {
+    case POSTER = 0;
+    case BANNER = 1;
+    case AVATAR = 2;
 
     public function label(): string {
         return strtolower($this->name);
