aaad15e97054b4d53c6839f4024954a5f638f550
aminnausin <aminnausin@outlook.com>
2026-05-29 01:37:24 -0400

    fix: series poster fk is wrong type

--- database/migrations/2026_05_29_042123_add_poster_to_series.php
+++ database/migrations/2026_05_29_042123_add_poster_to_series.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void {
         Schema::table('series', function (Blueprint $table) {
-            $table->uuid('primary_poster_id')->nullable()->constrained('images')->nullOnDelete();
+            $table->foreignId('primary_poster_id')->nullable()->constrained('images')->nullOnDelete();
         });
     }
 
