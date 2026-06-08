3797c8d496e85efdb96adfbe7b6cc13d7c390eb0
aminnausin <aminnausin@outlook.com>
2026-06-08 17:15:47 -0400

    feat: update preview generator service to use image service pipeline

--- app/Jobs/GeneratePreviewImage.php
+++ app/Jobs/GeneratePreviewImage.php
@@ -7,9 +7,10 @@ use App\Models\SubTask;
 use App\Services\GenerateImageException;
 use App\Services\PreviewGeneratorService;
 use App\Services\TaskService;
+use Illuminate\Database\Eloquent\Model;
 
 class GeneratePreviewImage extends ManagedSubTask {
-    protected $itemTitle;
+    protected string $itemTitle;
 
     public $timeout = 3600;
 
@@ -20,10 +21,10 @@ class GeneratePreviewImage extends ManagedSubTask {
      */
     public function __construct(
         public array $data,
-        public string $path,
+        public Model $owner,
         int $taskId,
     ) {
-        $this->itemTitle = $data['title'] ?? $path;
+        $this->itemTitle = $data['title'] ?? $owner->name ?? 'Unknown';
 
         $subTask = SubTask::create(['task_id' => $taskId, 'status' => TaskStatus::PENDING, 'name' => "Regenerate Preview Image for '{$this->itemTitle}'"]);
 
@@ -37,11 +38,11 @@ class GeneratePreviewImage extends ManagedSubTask {
         }
 
         try {
-            $result = $previewGenerator->generateImage($this->data, $this->path, true);
-            if (! $result) {
+            $image = $previewGenerator->generateAndPersist($this->owner, $this->data);
+            if (! $image) {
                 throw new GenerateImageException('Preview image generation failed. View logs for error.');
             }
-            $this->completeSubTask($taskService, 'Generated preview image.');
+            $this->completeSubTask($taskService, "Generated preview image at {$image->path}.");
         } catch (\Throwable $th) {
             $this->failSubTask($taskService, $th);
             throw $th;
