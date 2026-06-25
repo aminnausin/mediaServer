6c0dcc6f3974c3bd22fb4779ccbf69e4b7ba0022
aminnausin <aminnausin@outlook.com>
2026-06-25 00:26:41 -0400

    fix: folder info subtext wraps

--- resources/js/components/folders/FolderInfoRow.vue
+++ resources/js/components/folders/FolderInfoRow.vue
@@ -5,7 +5,7 @@ defineProps<{ title: string; value?: any }>();
     <div class="space-y-0.5">
         <div class="text-nowrap">{{ title }}</div>
         <slot>
-            <div class="text-foreground-1 text-xs" v-if="value">
+            <div class="text-foreground-1 text-xs text-nowrap" v-if="value">
                 {{ value }}
             </div>
         </slot>
