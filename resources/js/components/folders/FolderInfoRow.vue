f6444a1c7e6b9a75d887c4cf7cf502860ac37aa5
aminnausin <aminnausin@outlook.com>
2026-06-28 16:49:49 -0400

    feat: added more information to folder overview sidebar

--- resources/js/components/folders/FolderInfoRow.vue
+++ resources/js/components/folders/FolderInfoRow.vue
@@ -4,16 +4,17 @@ import { cn } from '@aminnausin/cedar-ui';
 
 import CircumShare1 from '~icons/circum/share-1';
 
-defineProps<{ title: string; value?: any; to?: string; valueClass?: string; iconClass?: string }>();
+defineProps<{ title: string; value?: any; to?: string; valueClass?: string; iconClass?: string; tooltip?: string }>();
 </script>
 <template>
-    <div class="space-y-0.5">
+    <div class="space-y-0.5" :title="$slots.default ? tooltip : undefined">
         <div class="text-nowrap">{{ title }}</div>
         <slot>
             <component
-                :class="cn('text-foreground-1 flex items-center gap-1 text-xs text-nowrap', { 'hover:text-primary dark:hover:text-primary-muted': to }, valueClass)"
+                :class="cn('text-foreground-1 flex items-center gap-1 text-xs', { 'hover:text-primary dark:hover:text-primary-muted': to }, valueClass)"
                 :is="to ? RouterLink : 'div'"
                 :to="to"
+                :title="tooltip"
             >
                 {{ value ?? '—' }}
                 <CircumShare1 v-if="to" :class="cn('size-4', iconClass)" />
--- resources/js/components/folders/FolderOverviewSidebar.vue
+++ resources/js/components/folders/FolderOverviewSidebar.vue
@@ -1,38 +1,85 @@
 <script setup lang="ts">
-import type { FolderResource } from '@/contracts/media';
-
-import { formatFileSize, toFormattedDuration } from '@/service/util';
-import { inject, useTemplateRef } from 'vue';
+import { formatFileSize, toFormattedDate, toFormattedDuration, toTimeSpan } from '@/service/util';
+import { computed, useTemplateRef } from 'vue';
 import { useScrollbarDetection } from '@/composables/design/useScrollbarDetection';
+import { useContentStore } from '@/stores/ContentStore';
+import { storeToRefs } from 'pinia';
 import { cn } from '@aminnausin/cedar-ui';
 
 import FolderInfoRow from '@/components/folders/FolderInfoRow.vue';
 import FolderTab from '@/components/folders/FolderTab.vue';
 
+const folderDatesScrollContainer = useTemplateRef('folder-dates');
 const folderInfoScrollContainer = useTemplateRef('folder-info');
-const data = inject<FolderResource>('data');
 
+const { hasScrollbar: folderDatesHasScrollbar } = useScrollbarDetection(folderDatesScrollContainer, 0, 'x');
 const { hasScrollbar: folderInfoHasScrollbar } = useScrollbarDetection(folderInfoScrollContainer, 0, 'x');
+const { stateFolder: stateFolder } = storeToRefs(useContentStore());
+
+const watchProgress = computed(() => `${stateFolder.value.videos.reduce((acc, vid) => (acc += vid.completion_count ? 1 : 0), 0)}/${stateFolder.value.file_count}`);
+const avgDuration = computed(() => toFormattedDuration(stateFolder.value.videos.reduce((acc, vid) => acc + (vid.duration ?? 0), 0) / (stateFolder.value.file_count || 1)));
+const totalViews = computed(() => stateFolder?.value.videos.reduce((acc, vid) => acc + (vid.view_count ?? 0), 0) ?? 0);
 </script>
 
 <template>
-    <FolderTab class="w-full @[40rem]:max-w-40">
-        <div v-if="data" :class="cn('scrollbar-minimal flex gap-x-4 gap-y-2 overflow-x-auto @[40rem]:flex-col', { 'pb-2': folderInfoHasScrollbar })" ref="folder-info">
-            <FolderInfoRow v-if="data.total_size" title="Total Size" :value="formatFileSize(data.total_size)" />
-            <FolderInfoRow title="Total Views" :value="data.videos.reduce((acc, vid) => (acc += vid.view_count ?? 0), 0)" />
-            <FolderInfoRow v-if="data.series?.episodes" title="Episodes" :value="data.series.episodes" />
-            <FolderInfoRow v-if="data.series?.seasons" title="Seasons" :value="data.series?.seasons" />
-            <FolderInfoRow title="Avg Duration" :value="toFormattedDuration(data.videos.reduce((acc, vid) => (acc += vid.duration ?? 0), 0) / (data.episodes || 1))" />
-            <FolderInfoRow title="Intro Duration" :value="`${data.series?.avg_intro_duration}s`" />
-            <FolderInfoRow v-if="data.series?.started_at" title="Start Date" :value="data.series?.started_at" />
-            <FolderInfoRow v-if="data.series?.ended_at" title="End Date" :value="data.series?.ended_at" />
-            <FolderInfoRow v-if="data.series?.rating" title="Average Score" :value="`${data.series?.rating}%`" />
-            <FolderInfoRow v-if="data.series?.studio" title="Studio" :value="data.series?.studio" />
-            <FolderInfoRow v-if="data.series?.folder_tags?.length" title="Tags">
-                <div class="flex gap-0.5 *:w-fit *:max-w-full *:truncate @[40rem]:flex-col">
-                    <div class="text-foreground-1 text-xs" :title="tag.name" v-for="tag in data.series?.folder_tags" :key="tag.id">{{ tag.name }}</div>
-                </div>
-            </FolderInfoRow>
-        </div>
-    </FolderTab>
+    <div class="flex w-full flex-col gap-2 @[40rem]:max-w-40">
+        <FolderTab>
+            <div :class="cn('scrollbar-minimal flex gap-x-4 gap-y-2 overflow-x-auto @[40rem]:flex-col', { 'pb-2': folderInfoHasScrollbar })" ref="folder-info">
+                <!-- Personal -->
+                <FolderInfoRow :title="'Progress'" :tooltip="`You have completed ${watchProgress} files`" :value="watchProgress" />
+                <FolderInfoRow title="Views" :value="totalViews" />
+
+                <!-- Files -->
+                <template v-if="stateFolder.series?.episodes">
+                    <FolderInfoRow v-if="stateFolder.series?.seasons" title="Seasons" :value="stateFolder.series?.seasons" />
+                    <FolderInfoRow
+                        title="Episodes"
+                        :value="stateFolder.file_count < stateFolder.series.episodes ? `${stateFolder.file_count}/${stateFolder.series?.episodes}` : stateFolder.series.episodes"
+                        :tooltip="stateFolder.file_count < stateFolder.series.episodes ? `${stateFolder.series?.episodes - stateFolder.file_count} files missing` : undefined"
+                    />
+                </template>
+                <FolderInfoRow v-else title="Files" :value="stateFolder.file_count" />
+
+                <FolderInfoRow title="Total Size" :value="formatFileSize(stateFolder.total_size)" />
+                <FolderInfoRow title="Avg Duration" :value="avgDuration" />
+                <FolderInfoRow title="Intro Duration" :value="`${stateFolder.series?.avg_intro_duration}s`" />
+
+                <!-- Metadata -->
+                <FolderInfoRow v-if="stateFolder.series?.rating !== null" title="Average Score" :value="`${stateFolder.series?.rating}%`" />
+                <FolderInfoRow v-if="stateFolder.series?.studio" title="Studios" :value="stateFolder.series?.studio" />
+                <FolderInfoRow v-if="stateFolder.series?.started_at" title="Start Date" :value="stateFolder.series?.started_at" />
+                <FolderInfoRow v-if="stateFolder.series?.ended_at" title="End Date" :value="stateFolder.series?.ended_at" />
+                <FolderInfoRow v-if="stateFolder.series?.folder_tags?.length" title="Tags">
+                    <div class="flex gap-0.5 *:w-fit *:max-w-full *:truncate @[40rem]:flex-col">
+                        <div class="text-foreground-1 text-xs" :title="tag.name" v-for="tag in stateFolder.series?.folder_tags" :key="tag.id">{{ tag.name }}</div>
+                    </div>
+                </FolderInfoRow>
+
+                <!-- Timestamps  -->
+            </div>
+        </FolderTab>
+        <FolderTab>
+            <div :class="cn('scrollbar-minimal flex gap-x-4 gap-y-2 overflow-x-auto @[40rem]:flex-col', { 'pb-2': folderDatesHasScrollbar })" ref="folder-dates">
+                <FolderInfoRow
+                    v-if="stateFolder.scanned_at"
+                    title="First Scanned"
+                    :value="toTimeSpan(stateFolder.scanned_at, '')"
+                    :tooltip="toFormattedDate(stateFolder.scanned_at)"
+                />
+                <FolderInfoRow
+                    v-if="stateFolder.series?.updated_at"
+                    title="Last Updated"
+                    :value="toTimeSpan(stateFolder.series?.updated_at, '')"
+                    :tooltip="toFormattedDate(stateFolder.series?.updated_at)"
+                />
+                <FolderInfoRow
+                    v-if="stateFolder.series?.edited_at"
+                    title="Last Edited"
+                    :value="toTimeSpan(stateFolder.series?.edited_at, '')"
+                    :tooltip="toFormattedDate(stateFolder.series?.edited_at)"
+                />
+                <FolderInfoRow title="Downloads" :value="stateFolder.series?.downloads_enabled ? 'Enabled' : 'Disabled'" />
+            </div>
+        </FolderTab>
+    </div>
 </template>
