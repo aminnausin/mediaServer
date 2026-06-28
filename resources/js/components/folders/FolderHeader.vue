ee492e396943b3d944d71269d9dd4259ded34060
aminnausin <aminnausin@outlook.com>
2026-06-28 15:25:57 -0400

    feat: skeleton ui for folder view

--- resources/js/components/folders/FolderHeader.vue
+++ resources/js/components/folders/FolderHeader.vue
@@ -1,33 +1,43 @@
 <script setup lang="ts">
-import type { FolderResource } from '@/contracts/media';
-
 import { handleStorageURL, toTimeSpan } from '@/service/util';
-import { inject } from 'vue';
+import { useContentStore } from '@/stores/ContentStore';
+import { storeToRefs } from 'pinia';
 
+import ProIconsPhotoOff from '@/components/icons/ProIconsPhotoOff.vue';
 import FolderTab from '@/components/folders/FolderTab.vue';
 import LazyImage from '@/components/lazy/LazyImage.vue';
 
-const data = inject<FolderResource>('data');
+const { stateFolder, isLoadingContent } = storeToRefs(useContentStore());
 </script>
 
 <template>
     <FolderTab class="h-fit w-full gap-0 p-0">
         <div
-            class="ring-r-default/5 flex h-52 items-end overflow-clip rounded-t-xl bg-cover text-white ring-1 lg:h-64"
+            class="ring-r-default/5 flex min-h-52 items-end overflow-clip rounded-t-xl bg-cover text-white ring-1 lg:h-64"
             :style="{
-                'background-image': `url(${data?.series?.banner_image?.path ?? 'https://s4.anilist.co/file/anilistcdn/user/banner/b6792701-mBLPRvzr3xPL.jpg'})`,
+                'background-image': `url(${stateFolder.series?.banner_image?.path ?? 'https://s4.anilist.co/file/anilistcdn/user/banner/b6792701-mBLPRvzr3xPL.jpg'})`,
             }"
         >
-            <div class="flex w-full flex-wrap items-end gap-4 bg-linear-to-b from-transparent to-neutral-950/40 p-3 text-center">
+            <div v-if="!isLoadingContent && stateFolder.id" class="flex w-full flex-wrap items-end gap-4 bg-linear-to-b from-transparent to-neutral-950/40 p-3 text-center">
                 <LazyImage
                     :wrapper-class="'relative h-fit w-fit'"
                     class="aspect-2-3 mx-auto w-24 min-w-24 rounded-md object-cover"
-                    :src="data?.series?.poster_image?.path ?? handleStorageURL(data?.series?.thumbnail_url) ?? '/storage/thumbnails/default.webp'"
+                    :src="stateFolder?.series?.poster_image?.path ?? handleStorageURL(stateFolder?.series?.thumbnail_url) ?? '/storage/thumbnails/default.webp'"
                     alt="profile"
                 />
                 <div class="text-centre flex flex-1 flex-wrap items-end justify-center gap-1 sm:pb-2">
-                    <h2 class="w-full text-2xl text-balance capitalize sm:flex-1 sm:text-left">{{ data?.title }}</h2>
-                    <p class="text-sm">Created: {{ toTimeSpan(data?.created_at || '', '') }}</p>
+                    <h2 class="w-full text-2xl text-balance capitalize sm:flex-1 sm:text-left">{{ stateFolder?.title }}</h2>
+                    <p class="text-sm">Created: {{ toTimeSpan(stateFolder?.created_at || '', '') }}</p>
+                </div>
+            </div>
+            <div v-else class="flex w-full flex-wrap items-end gap-4 bg-linear-to-b from-transparent to-neutral-950/40 p-3 text-center">
+                <div class="aspect-2-3 bg-surface-3 mx-auto flex w-24 min-w-24 items-center justify-center rounded-md">
+                    <ProIconsPhotoOff class="text-foreground-1 dark:text-foreground-0 size-6 animate-pulse" />
+                </div>
+
+                <div class="text-centre flex flex-1 animate-pulse flex-wrap items-end justify-center gap-1 sm:justify-between sm:pb-2">
+                    <div class="xs:mx-10 h-8 w-full rounded bg-white/80 text-2xl text-balance capitalize sm:mx-0 sm:max-w-1/3 sm:flex-1 sm:text-left"></div>
+                    <div class="h-5 w-20 rounded bg-neutral-200/80 sm:w-12"></div>
                 </div>
             </div>
         </div>
