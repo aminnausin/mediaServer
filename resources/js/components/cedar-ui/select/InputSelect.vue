98c86a7c3f40824eb99c6b8eb39bebf44fedc291
aminnausin <aminnausin@outlook.com>
2026-07-21 22:21:34 -0400

    fix: select truncates "by views"

--- resources/js/components/cedar-ui/select/InputSelect.vue
+++ resources/js/components/cedar-ui/select/InputSelect.vue
@@ -115,7 +115,7 @@ watch(
                     'flex items-center justify-between gap-2', // Layout
                     'cursor-pointer shadow-xs', // Style
                     'h-10 max-h-full w-full p-2 pl-3', // Size
-                    'rounded-md text-start break-all', // Shape
+                    'rounded-md text-start wrap-break-word', // Shape
                     'group',
                     props.class,
                 )
