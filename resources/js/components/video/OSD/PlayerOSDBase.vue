5880aad805d3243b06f08f89785eca9ae26d569d
aminnausin <aminnausin@outlook.com>
2026-09-13 22:42:51 -0400

    fix: player osd applies class twice

--- resources/js/components/video/OSD/PlayerOSDBase.vue
+++ resources/js/components/video/OSD/PlayerOSDBase.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { cn } from '@aminnausin/cedar-ui';
+
+defineOptions({ inheritAttrs: false });
 </script>
 <template>
-    <div :class="cn('flex items-center justify-center rounded-full border border-neutral-700/10 bg-neutral-900/60 p-3 text-white backdrop-blur-xs', $attrs.class)">
+    <div
+        v-bind="{ ...$attrs, class: undefined }"
+        :class="cn('flex items-center justify-center rounded-full border border-neutral-700/10 bg-neutral-900/60 p-3 text-white backdrop-blur-xs', $attrs.class)"
+    >
         <slot> </slot>
     </div>
 </template>
