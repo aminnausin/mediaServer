18cb4982dbe6a0dbce05bdbe8f83c6bc8ff5e206
aminnausin <aminnausin@outlook.com>
2026-09-17 19:19:00 -0400

    fix: local clipboard composable can't be used twice on a page

--- resources/js/composables/useClipboard.ts
+++ resources/js/composables/useClipboard.ts
@@ -3,21 +3,22 @@ import type { Ref } from 'vue';
 import { toast } from '@aminnausin/cedar-ui';
 import { ref } from 'vue';
 
-interface UseClipboard {
-    copyText: string;
-    copyNotification: boolean;
-    copyToClipboard: () => void;
-}
+export default function useClipboard(copyText?: Ref<string>) {
+    const copyNotification = ref<Set<string>>(new Set());
 
-export default function useClipboard(copyText: Ref<string>) {
-    const copyNotification = ref(false);
+    const copyToClipboard = async (text?: string, key?: string) => {
+        const payload = text ?? copyText?.value;
 
-    const copyToClipboard = async () => {
+        if (!payload) {
+            toast.warning('Nothing to copy.');
+            return;
+        }
+
         try {
-            await navigator.clipboard.writeText(copyText.value);
-            copyNotification.value = true;
+            await navigator.clipboard.writeText(payload);
+            copyNotification.value.add(key ?? payload);
             setTimeout(() => {
-                copyNotification.value = false;
+                copyNotification.value.delete(key ?? payload);
             }, 3000);
         } catch (error) {
             console.error(error);
